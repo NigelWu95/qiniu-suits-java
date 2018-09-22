42db922a81bf95c39d1fd3ceafa6da11756f74e4
wubingheng <wubinghengajw@outlook.com>
2018-09-23 07:15:05 +0800

    1. 产生两个字符的前缀时，会自动根据第一次单字符前缀列举时得到的响应 marker 设置是否需要递进双字符前缀。

--- src/main/java/com/qiniu/service/impl/ListBucketMultiProcess.java
+++ src/main/java/com/qiniu/service/impl/ListBucketMultiProcess.java
@@ -12,9 +12,11 @@ import com.qiniu.util.JSONConvertUtils;
 import com.qiniu.util.StringUtils;
 import com.qiniu.util.UrlSafeBase64;
 
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class ListBucketMultiProcess implements IBucketProcess {
 
@@ -43,25 +45,20 @@ public class ListBucketMultiProcess implements IBucketProcess {
         doMultiList("v2", withParallel, secondLevel);
     }
 
-    private Map<String, Integer> getDelimitedFileMap(String version, boolean secondLevel) {
-        List<String> prefixArray = new ArrayList<>();
-        List<String> prefixs = Arrays.asList("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(""));
-        prefixArray.add("");
-        prefixArray.addAll(prefixs);
-        Map<String, Integer> delimitedFileMap;
+    private Map<String, Integer[]> getDelimitedFileMap(String version, boolean secondLevel) {
+        List<String> prefixList = new ArrayList<>();
+        List<String> prefix = Arrays.asList("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(""));
+        prefixList.add("");
+        prefixList.addAll(prefix);
+        Map<String, Integer[]> delimitedFileMap;
 
         if (secondLevel) {
-            delimitedFileMap = listByPrefix(prefixArray, version, false);
-            System.out.println("1 " + prefixArray);
-            prefixArray = getSecondDelimitedFileMap(prefixArray, new ArrayList<>(delimitedFileMap.keySet()));
-            System.out.println("2 " + prefixArray);
-            prefixArray.add("");
-            System.out.println("3 " + prefixArray);
-            delimitedFileMap.putAll(listByPrefix(prefixArray, version, true));
-            System.out.println("4 " + delimitedFileMap.size());
+            delimitedFileMap = listByPrefix(prefixList, version, false);
+            prefixList = getSecondFilePrefix(prefixList, delimitedFileMap);
+            prefixList.add("");
+            delimitedFileMap.putAll(listByPrefix(prefixList, version, true));
         } else {
-            delimitedFileMap = listByPrefix(prefixArray, version, true);
-            System.out.println("1 " + delimitedFileMap.size());
+            delimitedFileMap = listByPrefix(prefixList, version, true);
         }
 
         // 原 bucketManager 实现中没有关闭单个请求的 response，修改实现使用类成员，使用完后统一关闭
@@ -70,8 +67,8 @@ public class ListBucketMultiProcess implements IBucketProcess {
         return delimitedFileMap;
     }
 
-    private Map<String, Integer> listByPrefix(List<String> prefixArray, String version, boolean doProcess) {
-        Map<String, Integer> delimitedFileMap = new LinkedHashMap<>();
+    private Map<String, Integer[]> listByPrefix(List<String> prefixArray, String version, boolean doProcess) {
+        Map<String, Integer[]> delimitedFileMap = new HashMap<>();
 
         for (int i = 0; i < prefixArray.size(); i++) {
             FileListing fileListing;
@@ -103,40 +100,45 @@ public class ListBucketMultiProcess implements IBucketProcess {
                 }
             }
 
-//            if (StringUtils.isNullOrEmpty(fileInfoAndMarker[1]))
-//                continue;
-            delimitedFileMap.put(fileKey, fileType);
+            Integer[] keyAttribute = new Integer[]{fileType, 1};
+
+            if (StringUtils.isNullOrEmpty(fileInfoAndMarker[1]))
+                keyAttribute[1] = 0;
+
+            delimitedFileMap.put(fileKey, keyAttribute);
         }
 
         return delimitedFileMap;
     }
 
-    private List<String> getSecondDelimitedFileMap(List<String> prefixArray, List<String> firstKeyList) {
+    private List<String> getSecondFilePrefix(List<String> prefixList, Map<String, Integer[]> delimitedFileMap) {
+        List<String> firstKeyList = new ArrayList<>(delimitedFileMap.keySet());
+        List<String> secondPrefixList = new ArrayList<>();
 
-        List<String> secondPrefixArray = new ArrayList<>();
-
-        for (int i = 0; i < firstKeyList.size(); i++) {
-            String firstPrefix = firstKeyList.get(i).substring(0, 1);
-            for (String secondPrefix : prefixArray) {
-                secondPrefixArray.add(firstPrefix + secondPrefix);
+        for (String firstKey : firstKeyList) {
+            String firstPrefix = firstKey.substring(0, 1);
+            if (delimitedFileMap.get(firstKey)[1] == 0) {
+                secondPrefixList.add(firstPrefix);
+                continue;
+            }
+            for (String secondPrefix : prefixList) {
+                secondPrefixList.add(firstPrefix + secondPrefix);
             }
         }
 
-        return secondPrefixArray;
+        return secondPrefixList;
     }
 
     private void doMultiList(String version, boolean withParallel, boolean secondLevel) {
 
-        Map<String, Integer> delimitedFileMap = getDelimitedFileMap(version, secondLevel);
+        Map<String, Integer[]> delimitedFileMap = getDelimitedFileMap(version, secondLevel);
         ExecutorService executorPool = Executors.newFixedThreadPool(threadNums);
         List<String> firstKeyList = new ArrayList<>(delimitedFileMap.keySet());
         Collections.sort(firstKeyList);
 
-        System.out.println(firstKeyList);
-
         for (int i = 0; i < firstKeyList.size(); i++) {
             String endFileKey = i == firstKeyList.size() - 1 ? "" : firstKeyList.get(i + 1);
-            String startMarker = UrlSafeBase64.encodeToString("{\"c\":" + delimitedFileMap.get(firstKeyList.get(i))
+            String startMarker = UrlSafeBase64.encodeToString("{\"c\":" + delimitedFileMap.get(firstKeyList.get(i))[0]
                     + ",\"k\":\"" + firstKeyList.get(i) + "\"}");
 
             executorPool.execute(() -> {
