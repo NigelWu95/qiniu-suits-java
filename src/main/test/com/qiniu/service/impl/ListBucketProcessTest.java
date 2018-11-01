f113104d78edf165f3c4599030c90d6918eeffbe
wubingheng <wubinghengajw@outlook.com>
2018-11-02 05:51:51 +0800

    1. debug test loop list by testStraightList method.

--- src/main/test/com/qiniu/service/impl/ListBucketProcessTest.java
+++ src/main/test/com/qiniu/service/impl/ListBucketProcessTest.java
@@ -1,5 +1,7 @@
 package com.qiniu.service.impl;
 
+import com.qiniu.common.ListFileAntiFilter;
+import com.qiniu.common.ListFileFilter;
 import com.qiniu.common.QiniuException;
 import com.qiniu.common.Zone;
 import com.qiniu.http.Response;
@@ -10,37 +12,58 @@ import com.qiniu.service.oss.ListBucket;
 import com.qiniu.storage.Configuration;
 import com.qiniu.util.Auth;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ListBucketProcessTest {
 
-    @Test
-    public void testGetListResult() throws Exception {
+    private ListBucketProcess listBucketProcess;
+    private ListBucket listBucket;
+
+    private Auth auth;
+    private Configuration configuration;
+    private String bucket;
+    private int unitLen;
+    private int version;
+
+    @Before
+    public void get() throws Exception {
         ListBucketParams listBucketParams = new ListBucketParams("resources/.qiniu.properties");
         String accessKey = listBucketParams.getAccessKey();
         String secretKey = listBucketParams.getSecretKey();
-        String bucket = listBucketParams.getBucket();
         String resultFileDir = listBucketParams.getResultFileDir();
-        int version = listBucketParams.getVersion();
-        int unitLen = listBucketParams.getUnitLen();
-        unitLen = (version == 1 && unitLen > 1000) ? unitLen%1000 : unitLen;
         String customPrefix = listBucketParams.getCustomPrefix();
         List<String> antiPrefix = listBucketParams.getAntiPrefix();
-        Auth auth = Auth.create(accessKey, secretKey);
-        Configuration configuration = new Configuration(Zone.autoZone());
-        ListBucket listBucket = new ListBucket(auth, configuration);
-        Response response = listBucket.run(bucket, "e", "", "", unitLen, 1, version);
-        ListBucketProcess listBucketProcess = new ListBucketProcess(auth, configuration, bucket, unitLen, version, resultFileDir,
+        this.bucket = listBucketParams.getBucket();
+        this.version = listBucketParams.getVersion();
+        this.unitLen = listBucketParams.getUnitLen();
+        this.unitLen = (version == 1 && unitLen > 1000) ? unitLen%1000 : unitLen;
+        this.auth = Auth.create(accessKey, secretKey);
+        this.configuration = new Configuration(Zone.autoZone());
+        this.listBucket = new ListBucket(auth, configuration);
+        this.listBucketProcess = new ListBucketProcess(auth, configuration, bucket, unitLen, version, resultFileDir,
                 customPrefix, antiPrefix, 1);
+    }
+
+    @Test
+    public void testGetListResult() throws Exception {
+        Response response = listBucket.run(bucket, "e", "", "", unitLen, 1, version);
         ListResult listResult = listBucketProcess.getListResult(response, version);
         response.close();
         Assert.assertTrue(listResult.isValid());
         Assert.assertEquals(1, listResult.fileInfoList.size());
+    }
+
+    @Test
+    public void testStraightList() throws IOException {
+        listBucketProcess.straightList("", "", "", null, false);
     }
 }
