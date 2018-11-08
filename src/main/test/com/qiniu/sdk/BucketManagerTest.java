2cc9d65d7cc0ed85494000d0cc59f96cd009e335
wubingheng <wubinghengajw@outlook.com>
2018-11-08 10:10:07 +0800

    1. update BucketManager and test.

---
+++ src/main/test/com/qiniu/sdk/BucketManagerTest.java
@@ -0,0 +1,12 @@
+package com.qiniu.sdk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BucketManagerTest {
+
+    @Test
+    public void listFilesV2() {
+    }
+}
--- src/main/test/com/qiniu/service/impl/ListBucketProcessTest.java
+++ src/main/test/com/qiniu/service/impl/ListBucketProcessTest.java
@@ -1,11 +1,7 @@
 package com.qiniu.service.impl;
 
-import com.qiniu.common.ListFileAntiFilter;
-import com.qiniu.common.ListFileFilter;
-import com.qiniu.common.QiniuException;
 import com.qiniu.common.Zone;
 import com.qiniu.http.Response;
-import com.qiniu.interfaces.IOssFileProcess;
 import com.qiniu.model.ListBucketParams;
 import com.qiniu.model.ListResult;
 import com.qiniu.service.oss.ListBucket;
@@ -15,30 +11,24 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class ListBucketProcessTest {
 
     private ListBucketProcess listBucketProcess;
     private ListBucket listBucket;
-
-    private Auth auth;
-    private Configuration configuration;
     private String bucket;
     private int unitLen;
     private int version;
 
     @Before
-    public void get() throws Exception {
+    public void init() throws Exception {
         ListBucketParams listBucketParams = new ListBucketParams("resources/.qiniu.properties");
         String accessKey = listBucketParams.getAccessKey();
         String secretKey = listBucketParams.getSecretKey();
+        Auth auth = Auth.create(accessKey, secretKey);
+        Configuration configuration = new Configuration(Zone.autoZone());
         String resultFileDir = listBucketParams.getResultFileDir();
         String customPrefix = listBucketParams.getCustomPrefix();
         List<String> antiPrefix = listBucketParams.getAntiPrefix();
@@ -46,8 +36,6 @@ public class ListBucketProcessTest {
         this.version = listBucketParams.getVersion();
         this.unitLen = listBucketParams.getUnitLen();
         this.unitLen = (version == 1 && unitLen > 1000) ? unitLen%1000 : unitLen;
-        this.auth = Auth.create(accessKey, secretKey);
-        this.configuration = new Configuration(Zone.autoZone());
         this.listBucket = new ListBucket(auth, configuration);
         this.listBucketProcess = new ListBucketProcess(auth, configuration, bucket, unitLen, version, resultFileDir,
                 customPrefix, antiPrefix, 1);
