f24120314c193bb812abeb498b739e2b08617a64
wubingheng <wubinghengajw@outlook.com>
2018-11-13 14:28:01 +0800

    update ListBucketProcess and test ListV2Line.

--- src/main/test/com/qiniu/service/oss/ListBucketTest.java
+++ src/main/test/com/qiniu/service/oss/ListBucketTest.java
@@ -34,7 +34,7 @@ public class ListBucketTest {
     public void testRun() {
         Response response;
         try {
-            response = listBucket.run(bucket, "v2/.6zer.com_2018-11-1", "", "", 1000, 3, 2);
+            response = listBucket.run(bucket, "", "", "", 1000, 3, 2);
             System.out.println(response.bodyString());
             response.close();
         } catch (QiniuException e) {
