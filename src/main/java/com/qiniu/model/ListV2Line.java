9e5402a45ae742093f4217a918c1c3edd9d2841d
wubingheng <wubinghengajw@outlook.com>
2018-11-13 15:39:21 +0800

    update ListV2Line to fix FileInfo compare by key.

--- src/main/java/com/qiniu/model/ListV2Line.java
+++ src/main/java/com/qiniu/model/ListV2Line.java
@@ -1,7 +1,10 @@
 package com.qiniu.model;
 
+import com.google.gson.JsonObject;
 import com.qiniu.storage.model.FileInfo;
+import com.qiniu.util.JsonConvertUtils;
 import com.qiniu.util.StringUtils;
+import com.qiniu.util.UrlSafeBase64;
 
 public class ListV2Line {
 
@@ -16,10 +19,17 @@ public class ListV2Line {
             return 0;
         } else if (this.fileInfo == null) {
             if (!"".equals(marker)) {
-
+                String markerJson = new String(UrlSafeBase64.decode(marker));
+                String key = JsonConvertUtils.fromJson(markerJson, JsonObject.class).get("k").getAsString();
+                return key.compareTo(listV2Line.fileInfo.key);
             }
             return 1;
         } else if (listV2Line.fileInfo == null) {
+            if (!"".equals(listV2Line.marker)) {
+                String markerJson = new String(UrlSafeBase64.decode(listV2Line.marker));
+                String key = JsonConvertUtils.fromJson(markerJson, JsonObject.class).get("k").getAsString();
+                return this.fileInfo.key.compareTo(key);
+            }
             return -1;
         } else {
             return this.fileInfo.key.compareTo(listV2Line.fileInfo.key);
