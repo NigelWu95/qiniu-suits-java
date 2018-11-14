699e7e238654e1c12e2f6702e1a501e6144e8fc7
wubingheng <wubinghengajw@outlook.com>
2018-11-14 09:07:32 +0800

    update entry.

---
+++ src/main/java/com/qiniu/entries/EntryMain.java
@@ -0,0 +1,35 @@
+package com.qiniu.entries;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntryMain {
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> configFiles = new ArrayList<String>(){{
+            add("resources/qiniu.properties");
+            add("resources/.qiniu.properties");
+        }};
+        boolean paramFromConfig = true;
+        if (args != null && args.length > 0) {
+            if (args[0].startsWith("-config=")) configFiles.add(args[0].split("=")[1]);
+            else paramFromConfig = false;
+        }
+        String configFilePath = null;
+        if (paramFromConfig) {
+            for (int i = configFiles.size() - 1; i >= 0; i--) {
+                File file = new File(configFiles.get(i));
+                if (file.exists()) {
+                    configFilePath = configFiles.get(i);
+                    break;
+                }
+            }
+            if (configFilePath == null) throw new Exception("there is no config file detected.");
+            else paramFromConfig = true;
+        }
+
+        ListBucketMain.runMain(paramFromConfig, args, configFilePath);
+    }
+}
--- src/main/java/com/qiniu/entries/ListBucketMain.java
+++ src/main/java/com/qiniu/entries/ListBucketMain.java
@@ -7,35 +7,11 @@ import com.qiniu.service.impl.*;
 import com.qiniu.storage.Configuration;
 import com.qiniu.util.Auth;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ListBucketMain {
 
-    public static void main(String[] args) throws Exception {
-
-        List<String> configFiles = new ArrayList<String>(){{
-            add("resources/qiniu.properties");
-            add("resources/.qiniu.properties");
-        }};
-        boolean paramFromConfig = true;
-        if (args != null && args.length > 0) {
-            if (args[0].startsWith("-config=")) configFiles.add(args[0].split("=")[1]);
-            else paramFromConfig = false;
-        }
-        String configFilePath = null;
-        if (paramFromConfig) {
-            for (int i = configFiles.size() - 1; i >= 0; i--) {
-                File file = new File(configFiles.get(i));
-                if (file.exists()) {
-                    configFilePath = configFiles.get(i);
-                    break;
-                }
-            }
-            if (configFilePath == null) throw new Exception("there is no config file detected.");
-            else paramFromConfig = true;
-        }
+    public static void runMain(boolean paramFromConfig, String[] args, String configFilePath) throws Exception {
 
         ListBucketParams listBucketParams = paramFromConfig ? new ListBucketParams(configFilePath) : new ListBucketParams(args);
         String accessKey = listBucketParams.getAccessKey();
