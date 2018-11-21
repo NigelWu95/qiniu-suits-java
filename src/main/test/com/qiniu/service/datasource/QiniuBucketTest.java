6dbb96634153213f9323d5ea48d7cbd3e6842304
wubingheng <wubinghengajw@outlook.com>
2018-11-22 00:21:35 +0800

    update QiniuBucket and QiniuBucketTest.

---
+++ src/main/test/com/qiniu/service/datasource/QiniuBucketTest.java
@@ -0,0 +1,53 @@
+package com.qiniu.service.datasource;
+
+import com.qiniu.common.Zone;
+import com.qiniu.model.ListBucketParams;
+import com.qiniu.sdk.BucketManager;
+import com.qiniu.storage.Configuration;
+import com.qiniu.util.Auth;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class QiniuBucketTest {
+
+    private QiniuBucket qiniuBucket;
+    private String bucket;
+    private int unitLen;
+    private int version;
+
+    @Before
+    public void init() throws Exception {
+        ListBucketParams listBucketParams = new ListBucketParams("resources/.qiniu.properties");
+        String accessKey = listBucketParams.getAccessKey();
+        String secretKey = listBucketParams.getSecretKey();
+        Auth auth = Auth.create(accessKey, secretKey);
+        Configuration configuration = new Configuration(Zone.autoZone());
+        String customPrefix = listBucketParams.getCustomPrefix();
+        List<String> antiPrefix = listBucketParams.getAntiPrefix();
+        this.bucket = listBucketParams.getBucket();
+        this.version = listBucketParams.getVersion();
+        this.unitLen = listBucketParams.getUnitLen();
+        this.unitLen = (version == 1 && unitLen > 1000) ? unitLen%1000 : unitLen;
+        this.qiniuBucket = new QiniuBucket(auth, configuration, bucket, unitLen, version, customPrefix,
+                antiPrefix, 1);
+    }
+
+    @Test
+    public void testConcurrentlyList() {
+        qiniuBucket.concurrentlyList(10, 2, null);
+    }
+
+    @Test
+    public void testCheckValidPrefix() {
+        qiniuBucket.checkValidPrefix(1);
+    }
+
+    @Test
+    public void testStraightlyList() {
+        qiniuBucket.straightlyList(null, null, null);
+    }
+}
