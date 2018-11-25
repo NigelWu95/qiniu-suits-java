8f5276eb86a96ff801a4955f384c940d71e76e01
wubingheng <wubinghengajw@outlook.com>
2018-11-26 00:24:47 +0800

    add stat method in FileChecker.

--- src/main/java/com/qiniu/service/qoss/FileChecker.java
+++ src/main/java/com/qiniu/service/qoss/FileChecker.java
@@ -5,6 +5,7 @@ import com.qiniu.common.QiniuException;
 import com.qiniu.http.Client;
 import com.qiniu.http.Response;
 import com.qiniu.model.qoss.Qhash;
+import com.qiniu.storage.model.FileInfo;
 import com.qiniu.util.JsonConvertUtils;
 import com.qiniu.util.RequestUtils;
 
@@ -73,5 +74,60 @@ public class FileChecker {
             throw new QiniuException(e, e.getMessage());
         }
         return qhash;
+    }
+
+    public FileInfo getStat(String url) throws QiniuException, UnknownHostException {
+
+        String[] addr = url.split("/");
+        if (addr.length < 3) throw new QiniuException(null, "not valid url.");
+        String domain = addr[2];
+        RequestUtils.checkHost(domain);
+        StringBuilder key = new StringBuilder();
+        for (int i = 3; i < addr.length; i++) {
+            key.append(addr[i]).append("/");
+        }
+        return getStat(domain, key.toString().substring(0, key.length() - 1));
+    }
+
+    public FileInfo getStat(String domain, String sourceKey) throws QiniuException {
+
+        return getStatByJson(getStatJson(domain, sourceKey)) ;
+    }
+
+    /**
+     *
+     * @param domain
+     * @param sourceKey
+     * @return
+     * @throws QiniuException
+     */
+    public JsonObject getStatJson(String domain, String sourceKey) throws QiniuException {
+
+        try {
+            RequestUtils.checkHost(domain);
+        } catch (UnknownHostException e) {
+            throw new QiniuException(e);
+        }
+        String url = "http://" + domain + "/" + sourceKey.split("\\?")[0];
+        Response response = client.get(url + "?stat");
+        JsonObject fileInfoJson = JsonConvertUtils.toJsonObject(response.bodyString());
+        response.close();
+        return fileInfoJson;
+    }
+
+    public FileInfo getStatByJson(String fileInfoJson) throws QiniuException {
+
+        return getStatByJson(JsonConvertUtils.toJsonObject(fileInfoJson));
+    }
+
+    public FileInfo getStatByJson(JsonObject fileInfoJson) throws QiniuException {
+
+        FileInfo fileInfo;
+        try {
+            fileInfo = JsonConvertUtils.fromJson(fileInfoJson, FileInfo.class);
+        } catch (JsonParseException e) {
+            throw new QiniuException(e, e.getMessage());
+        }
+        return fileInfo;
     }
 }
---
+++ src/main/java/com/qiniu/service/qoss/FileStat.java
@@ -0,0 +1,4 @@
+package com.qiniu.service.qoss;
+
+public class FileStat {
+}
