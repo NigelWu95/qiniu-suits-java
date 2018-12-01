48cd1cfbd2524a6fc59330fb16e503930c64cf07
wubingheng <wubinghengajw@outlook.com>
2018-12-01 10:45:15 +0800

    update custom AvinfoProcess.

--- src/main/java/com/qiniu/util/ObjectUtils.java
+++ src/main/java/com/qiniu/util/ObjectUtils.java
@@ -1,5 +1,7 @@
 package com.qiniu.util;
 
+import com.qiniu.common.QiniuException;
+
 public class ObjectUtils {
 
     public static String addSuffix(String name, String suffix) {
@@ -10,27 +12,28 @@ public class ObjectUtils {
         return prefix + name;
     }
 
-    public static String addPrefixAndSuffixKeepExt(String prefix, String name, String suffix) {
+    public static String addPrefixAndSuffixKeepExt(String prefix, String name, String suffix) throws QiniuException {
 
         return prefix + addSuffixKeepExt(name, suffix);
     }
 
-    public static String addSuffixKeepExt(String name, String suffix) {
+    public static String addSuffixKeepExt(String name, String suffix) throws QiniuException {
 
         return addSuffixWithExt(name, suffix, null);
     }
 
-    public static String addPrefixAndSuffixWithExt(String prefix, String name, String suffix, String ext) {
+    public static String addPrefixAndSuffixWithExt(String prefix, String name, String suffix, String ext)
+            throws QiniuException{
         return prefix + addSuffixWithExt(name, suffix, ext);
     }
 
-    public static String replaceExt(String name, String ext) {
+    public static String replaceExt(String name, String ext) throws QiniuException {
         return addSuffixWithExt(name, "", ext);
     }
 
-    public static String addSuffixWithExt(String name, String suffix, String ext) {
+    public static String addSuffixWithExt(String name, String suffix, String ext) throws QiniuException {
 
-        if (name == null) return "null" + suffix + "." + ext;
+        if (name == null || "".equals(name)) throw new QiniuException(null, "the name is empty");
         String[] names = name.split("\\.");
         if (names.length < 2) return name + suffix;
         else {
