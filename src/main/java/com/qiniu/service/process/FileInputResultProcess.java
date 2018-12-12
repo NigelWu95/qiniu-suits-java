bed02d24acb63322825aec9ad3edc6ad52855e33
wubingheng <wubinghengajw@outlook.com>
2018-12-13 05:49:36 +0800

    update.

--- src/main/java/com/qiniu/service/process/FileInputResultProcess.java
+++ src/main/java/com/qiniu/service/process/FileInputResultProcess.java
@@ -3,54 +3,37 @@ package com.qiniu.service.process;
 import com.qiniu.common.QiniuException;
 import com.qiniu.model.parameter.InfoMapParams;
 import com.qiniu.persistence.FileMap;
-import com.qiniu.service.convert.FileLineToMap;
-import com.qiniu.service.convert.InfoMapToString;
 import com.qiniu.service.interfaces.ILineProcess;
-import com.qiniu.service.interfaces.ITypeConvert;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class FileInputResultProcess implements ILineProcess<String>, Cloneable {
+public class FileInputResultProcess implements ILineProcess<Map<String, String>>, Cloneable {
 
     private String processName;
-    private String parserType;
-    private String inputSeparator;
     private InfoMapParams infoMapParams;
     private String resultFormat;
     private String separator;
     private String resultFileDir;
-    private boolean saveTotal = false;
     private FileMap fileMap;
-    private ITypeConvert<String, Map<String, String>> typeConverter;
-    private ITypeConvert<Map<String, String>, String> writeTypeConverter;
     private ILineProcess<Map<String, String>> nextProcessor;
 
     private void initBaseParams() {
         this.processName = "input";
     }
 
-    public FileInputResultProcess(String parserType, String inputSeparator, InfoMapParams infoMapParams,
-                                  String resultFormat, String separator, String resultFileDir, boolean saveTotal) {
+    public FileInputResultProcess(String resultFormat, String separator, String resultFileDir) {
         initBaseParams();
-        this.parserType = parserType;
-        this.inputSeparator = (inputSeparator == null || "".equals(inputSeparator)) ? "\t" : inputSeparator;
-        this.infoMapParams = infoMapParams;
-        this.resultFormat = resultFormat;
-        this.separator = separator;
+        this.resultFormat = (resultFormat == null || "".equals(resultFormat)) ? "json" : resultFormat;
+        this.separator = (separator == null || "".equals(separator)) ? "\t" : separator;
         this.resultFileDir = resultFileDir;
-        this.saveTotal = saveTotal;
         this.fileMap = new FileMap();
-        this.typeConverter = new FileLineToMap(parserType, inputSeparator, infoMapParams);
-        this.writeTypeConverter = new InfoMapToString(resultFormat, separator, true, true, true,
-                true, true, true, true);
     }
 
-    public FileInputResultProcess(String parserType, String inputSeparator, InfoMapParams infoMapParams,
-                                  String resultFormat, String separator, String resultFileDir, boolean saveTotal,
-                                  int resultFileIndex) throws IOException {
-        this(parserType, inputSeparator, infoMapParams, resultFormat, separator, resultFileDir, saveTotal);
+    public FileInputResultProcess(String resultFormat, String separator, String resultFileDir, int resultFileIndex)
+            throws IOException {
+        this(resultFormat, separator, resultFileDir);
         fileMap.initWriter(resultFileDir, processName, resultFileIndex);
     }
 
@@ -63,9 +46,6 @@ public class FileInputResultProcess implements ILineProcess<String>, Cloneable {
         fileInputResultProcess.fileMap = new FileMap();
         try {
             fileInputResultProcess.fileMap.initWriter(resultFileDir, processName, resultFileIndex);
-            fileInputResultProcess.typeConverter = new FileLineToMap(parserType, inputSeparator, infoMapParams);
-            fileInputResultProcess.writeTypeConverter = new InfoMapToString(resultFormat, separator, true,
-                    true, true, true, true, true, true);
             if (nextProcessor != null) {
                 fileInputResultProcess.nextProcessor = nextProcessor.getNewInstance(resultFileIndex);
             }
@@ -79,13 +59,9 @@ public class FileInputResultProcess implements ILineProcess<String>, Cloneable {
         return processName;
     }
 
-    public void processLine(List<String> fileLineList) throws QiniuException {
-        if (fileLineList == null || fileLineList.size() == 0) return;
-        List<Map<String, String>> infoMapList = typeConverter.convertToVList(fileLineList);
+    public void processLine(List<Map<String, String>> infoMapList) throws QiniuException {
+        if (infoMapList == null || infoMapList.size() == 0) return;
         try {
-            if (saveTotal) {
-                fileMap.writeSuccess(String.join("\n", writeTypeConverter.convertToVList(infoMapList)));
-            }
             if (nextProcessor != null) nextProcessor.processLine(infoMapList);
         } catch (Exception e) {
             throw new QiniuException(e, e.getMessage());
