72024127bd325b005bc7866062cd53fe7c4d032f
wubingheng <wubinghengajw@outlook.com>
2018-12-23 23:42:32 +0800

    optimise the typeConverter.

--- src/main/java/com/qiniu/service/convert/FileInfoToString.java
+++ src/main/java/com/qiniu/service/convert/FileInfoToString.java
@@ -14,6 +14,7 @@ public class FileInfoToString implements ITypeConvert<FileInfo, String> {
 
     private IStringFormat<FileInfo> stringFormatter;
     private  List<String> usedFields;
+    volatile private List<String> errorList = new ArrayList<>();
 
     public FileInfoToString(String format, String separator, List<String> usedFields) throws IOException {
         if (usedFields == null || usedFields.size() == 0) throw new IOException("there are no fields be set.");
@@ -25,11 +26,26 @@ public class FileInfoToString implements ITypeConvert<FileInfo, String> {
         }
     }
 
-    public List<String> convertToVList(List<FileInfo> srcList) {
+    public List<String> convertToVList(List<FileInfo> srcList) throws IOException {
         if (srcList == null || srcList.size() == 0) return new ArrayList<>();
-        return srcList.parallelStream()
+        List<String> resultList = srcList.parallelStream()
                 .filter(Objects::nonNull)
-                .map(fileInfo -> stringFormatter.toFormatString(fileInfo, usedFields))
+                .map(fileInfo -> {
+                    try {
+                        return stringFormatter.toFormatString(fileInfo, usedFields);
+                    } catch (Exception e) {
+                        errorList.add(fileInfo.key + "\t" + String.valueOf(fileInfo));
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+        if (errorList.size() == srcList.size()) throw new IOException("covert map by fields failed, " +
+                "please check the save fields' setting.");
+        return resultList;
+    }
+
+    public List<String> getErrorList() {
+        return errorList;
     }
 }
--- src/main/java/com/qiniu/service/fileline/FileInfoJsonFormatter.java
+++ src/main/java/com/qiniu/service/fileline/FileInfoJsonFormatter.java
@@ -4,11 +4,12 @@ import com.google.gson.JsonObject;
 import com.qiniu.service.interfaces.IStringFormat;
 import com.qiniu.storage.model.FileInfo;
 
+import java.io.IOException;
 import java.util.List;
 
 public class FileInfoJsonFormatter implements IStringFormat<FileInfo> {
 
-    public String toFormatString(FileInfo fileInfo, List<String> usedFields) {
+    public String toFormatString(FileInfo fileInfo, List<String> usedFields) throws IOException {
         JsonObject converted = new JsonObject();
         usedFields.forEach(key-> {
             switch (key) {
@@ -21,6 +22,7 @@ public class FileInfoJsonFormatter implements IStringFormat<FileInfo> {
                 case "status": converted.addProperty(key, fileInfo.status); break;
             }
         });
+        if (converted.size() == 0) throw new IOException("there are no valid file info key in fields.");
         return converted.toString();
     }
 }
