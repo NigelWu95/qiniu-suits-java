a69665904952aae942a4e511b7fced253318eda2
wubingheng <wubinghengajw@outlook.com>
2018-12-23 11:58:22 +0800

    remove custom  BucketManager and update qos process.

--- src/main/java/com/qiniu/service/qoss/FileStat.java
+++ src/main/java/com/qiniu/service/qoss/FileStat.java
@@ -1,11 +1,12 @@
 package com.qiniu.service.qoss;
 
 import com.qiniu.common.QiniuException;
-import com.qiniu.http.Response;
-import com.qiniu.sdk.BucketManager.*;
+import com.qiniu.storage.BucketManager.*;
 import com.qiniu.service.interfaces.ILineProcess;
 import com.qiniu.storage.Configuration;
+import com.qiniu.storage.model.FileInfo;
 import com.qiniu.util.Auth;
+import com.qiniu.util.JsonConvertUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,12 +24,13 @@ public class FileStat extends OperationBase implements ILineProcess<Map<String, 
         this(auth, configuration, bucket, resultPath, 0);
     }
 
-    protected Response getResponse(Map<String, String> fileInfo) throws QiniuException {
-        return bucketManager.statResponse(bucket, fileInfo.get("key"));
+    protected String processLine(Map<String, String> line) throws QiniuException {
+        FileInfo result = bucketManager.stat(bucket, line.get("key"));
+        return JsonConvertUtils.toJsonWithoutUrlEscape(result);
     }
 
-    synchronized protected BatchOperations getOperations(List<Map<String, String>> fileInfoList) {
-        List<String> keyList = fileInfoList.stream().map(fileInfo -> fileInfo.get("key")).collect(Collectors.toList());
+    synchronized protected BatchOperations getOperations(List<Map<String, String>> lineList) {
+        List<String> keyList = lineList.stream().map(line -> line.get("key")).collect(Collectors.toList());
         return batchOperations.addStatOps(bucket, keyList.toArray(new String[]{}));
     }
 
