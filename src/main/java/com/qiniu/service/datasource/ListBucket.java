b3b8ff585c64fb23f1c407d5f07334d75aa4d3d3
wubingheng <wubinghengajw@outlook.com>
2019-01-01 23:55:48 +0800

    optimise ListBucket.

--- src/main/java/com/qiniu/service/datasource/ListBucket.java
+++ src/main/java/com/qiniu/service/datasource/ListBucket.java
@@ -99,53 +99,36 @@ public class ListBucket {
     }
 
     private List<FileLister> getFileListerList(int unitLen) {
-        List<String> validPrefixList = originPrefixList.parallelStream()
-                .filter(originPrefix -> !antiPrefix.contains(originPrefix))
-                .map(prefix -> cPrefix + prefix)
-                .collect(Collectors.toList());
+        List<String> validPrefixList = originPrefixList.parallelStream().filter(originPrefix ->
+                !antiPrefix.contains(originPrefix)).map(prefix -> cPrefix + prefix).collect(Collectors.toList());
         List<FileLister> fileListerList = prefixList(validPrefixList, unitLen);
         Map<Boolean, List<FileLister>> groupedFileListerMap;
         while (fileListerList.size() < maxThreads - 1 && fileListerList.size() > 0) {
-            groupedFileListerMap = fileListerList.stream().collect(Collectors.groupingBy(
-                            fileLister -> fileLister.getMarker() != null && !"".equals(fileLister.getMarker())
-                    ));
+            groupedFileListerMap = fileListerList.stream().collect(Collectors.groupingBy(fileLister ->
+                    fileLister.getMarker() != null && !"".equals(fileLister.getMarker())));
             if (groupedFileListerMap.get(true) != null) {
-                Optional<List<String>> listOptional = groupedFileListerMap.get(true).parallelStream()
-                        .map(fileLister -> {
-                            List<FileInfo> list = fileLister.getFileInfoList();
-                            String point = "";
-                            if (list != null && list.size() > 0) {
-                                int prefixLen = fileLister.getPrefix().length();
-                                point = list.get(0).key.substring(prefixLen, prefixLen + 1);
-                            }
-                            String finalPoint = point;
-                            return originPrefixList.stream()
-                                    .filter(prefix -> prefix.compareTo(finalPoint) >= 0)
-                                    .map(originPrefix -> fileLister.getPrefix() + originPrefix)
-                                    .collect(Collectors.toList());
-                        })
-                        .reduce((list1, list2) -> { list1.addAll(list2); return list1; });
+                Optional<List<String>> listOptional = groupedFileListerMap.get(true).parallelStream().map(fileLister ->
+                {
+                    List<FileInfo> list = fileLister.getFileInfoList();
+                    String point = "";
+                    if (list != null && list.size() > 0) {
+                        int prefixLen = fileLister.getPrefix().length();
+                        point = list.get(0).key.substring(prefixLen, prefixLen + 1);
+                    }
+                    String finalPoint = point;
+                    return originPrefixList.stream()
+                            .filter(prefix -> prefix.compareTo(finalPoint) >= 0)
+                            .map(originPrefix -> fileLister.getPrefix() + originPrefix).collect(Collectors.toList());
+                }).reduce((list1, list2) -> { list1.addAll(list2); return list1; });
                 if (listOptional.isPresent()) {
                     validPrefixList = listOptional.get().parallelStream()
-                            .filter(originPrefix -> !antiPrefix.contains(originPrefix))
-                            .collect(Collectors.toList());
+                            .filter(originPrefix -> !antiPrefix.contains(originPrefix)).collect(Collectors.toList());
                     fileListerList = prefixList(validPrefixList, unitLen);
                 }
             } else {
                 break;
             }
             if (groupedFileListerMap.get(false) != null) fileListerList.addAll(groupedFileListerMap.get(false));
-            String firstEnd = fileListerList.get(0).getPrefix();
-            fileListerList.get(0).setEndKeyPrefix(firstEnd);
-                FileLister fileLister = fileListerList.get(fileListerList.size() -1);
-                if (StringUtils.isNullOrEmpty(fileLister.getMarker())) {
-                    FileInfo lastFileInfo = fileLister.getFileInfoList().parallelStream()
-                            .filter(Objects::nonNull)
-                            .max(Comparator.comparing(fileInfo -> fileInfo.key))
-                            .orElse(null);
-                    String marker = ListBucketUtils.calcMarker(lastFileInfo);
-                    fileLister.setMarker(marker);
-            }
         }
         // 加入第一段 FileLister，第一段 Lister 使用的 prefix 为 cPrefix（空或者传入的参数）
         fileListerList.addAll(prefixList(new ArrayList<String>(){{add(cPrefix);}}, unitLen));
