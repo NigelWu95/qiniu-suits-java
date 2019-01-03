76d2f94bae795151d8dd3a44e1a33225c39970e5
wubingheng <wubinghengajw@outlook.com>
2019-01-03 10:37:37 +0800

    update.

--- src/main/java/com/qiniu/model/parameter/FileCopyParams.java
+++ src/main/java/com/qiniu/model/parameter/FileCopyParams.java
@@ -7,12 +7,14 @@ public class FileCopyParams extends QossParams {
     private String targetBucket;
     private String keepKey;
     private String keyPrefix;
+    private String rmPrefix;
 
     public FileCopyParams(IEntryParam entryParam) throws Exception {
         super(entryParam);
         this.targetBucket = entryParam.getParamValue("to-bucket");
         try { this.keepKey = entryParam.getParamValue("keep-key"); } catch (Exception e) { keepKey = ""; }
         try { this.keyPrefix = entryParam.getParamValue("add-prefix"); } catch (Exception e) { keyPrefix = ""; }
+        try { this.rmPrefix = entryParam.getParamValue("rm-prefix"); } catch (Exception e) { rmPrefix = ""; }
     }
 
     public String getTargetBucket() {
@@ -29,5 +31,9 @@ public class FileCopyParams extends QossParams {
 
     public String getKeyPrefix() {
         return keyPrefix;
+    }
+
+    public String getRmPrefix() {
+        return rmPrefix;
     }
 }
--- src/main/java/com/qiniu/service/qoss/CopyFile.java
+++ src/main/java/com/qiniu/service/qoss/CopyFile.java
@@ -16,36 +16,35 @@ import java.util.stream.Collectors;
 public class CopyFile extends OperationBase implements ILineProcess<Map<String, String>>, Cloneable {
 
     private String toBucket;
-    private boolean keepKey;
     private String keyPrefix;
+    private String rmPrefix;
 
-    public CopyFile(Auth auth, Configuration configuration, String bucket, String toBucket, boolean keepKey,
-                    String keyPrefix, String resultPath, int resultIndex) throws IOException {
+    public CopyFile(Auth auth, Configuration configuration, String bucket, String toBucket, String keyPrefix,
+                    String rmPrefix, String resultPath, int resultIndex) throws IOException {
         super(auth, configuration, bucket, "copy", resultPath, resultIndex);
         this.toBucket = toBucket;
-        this.keepKey = keepKey;
         this.keyPrefix = keyPrefix == null ? "" : keyPrefix;
+        this.rmPrefix = rmPrefix == null ? "" : rmPrefix;
     }
 
-    public CopyFile(Auth auth, Configuration configuration, String bucket, String toBucket, boolean keepKey,
-                    String keyPrefix, String resultPath) throws IOException {
-        this(auth, configuration, bucket, toBucket, keepKey, keyPrefix, resultPath, 0);
+    public CopyFile(Auth auth, Configuration configuration, String bucket, String toBucket, String keyPrefix,
+                    String rmPrefix, String resultPath) throws IOException {
+        this(auth, configuration, bucket, toBucket, keyPrefix, rmPrefix, resultPath, 0);
+    }
+
+    private String formatKey(String key) {
+        return keyPrefix + key.substring(0, rmPrefix.length()).replace(rmPrefix, "")
+                + key.substring(rmPrefix.length());
     }
 
     protected String processLine(Map<String, String> line) throws QiniuException {
-        Response response = bucketManager.copy(bucket, line.get("key"), toBucket, keepKey ? keyPrefix +
-                line.get("key") : null, false);
+        Response response = bucketManager.copy(bucket, line.get("key"), toBucket, formatKey(line.get("key")), false);
         return response.statusCode + "\t" + HttpResponseUtils.getResult(response);
     }
 
     synchronized protected BatchOperations getOperations(List<Map<String, String>> lineList) {
         List<String> keyList = lineList.stream().map(line -> line.get("key")).collect(Collectors.toList());
-        if (keepKey) {
-            keyList.forEach(fileKey -> batchOperations.addCopyOp(bucket, fileKey, toBucket, keyPrefix + fileKey));
-        } else {
-            keyList.forEach(fileKey -> batchOperations.addCopyOp(bucket, fileKey, toBucket, null));
-        }
-
+        keyList.forEach(fileKey -> batchOperations.addCopyOp(bucket, fileKey, toBucket, formatKey(fileKey)));
         return batchOperations;
     }
 }
