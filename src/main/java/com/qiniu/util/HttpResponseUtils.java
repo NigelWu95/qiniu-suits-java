aa105213c15875debc34af72ca21d4dc291e9563
wubingheng <wubinghengajw@outlook.com>
2019-01-07 17:26:46 +0800

    update exception process in process.

--- src/main/java/com/qiniu/util/HttpResponseUtils.java
+++ src/main/java/com/qiniu/util/HttpResponseUtils.java
@@ -5,6 +5,8 @@ import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HttpResponseUtils {
 
@@ -29,17 +31,31 @@ public class HttpResponseUtils {
         }
     }
 
-    public static void processException(QiniuException e, FileMap fileMap, String info) throws IOException {
+    public static void processException(QiniuException e, FileMap fileMap, List<String> infoList) throws IOException {
         if (e != null) {
             if (e.response != null) {
-                if (fileMap != null) fileMap.writeError(e.response.reqId + "\t" + info + "\t" + e.error());
-                if (e.response.needSwitchServer() || e.response.statusCode == 631 || e.response.statusCode == 640) {
+                if (fileMap != null) {
+                    if (infoList == null || infoList.size() == 0)
+                        fileMap.writeError(e.response.reqId + "\t" + e.error());
+                    else
+                        fileMap.writeError(String.join("\n", infoList.stream()
+                                .map(line -> e.response.reqId + "\t" + line + "\t" + e.error())
+                                .collect(Collectors.toList())));
+                }
+                if (e.response.needSwitchServer() || e.response.statusCode >= 630) {
                     throw e;
                 } else {
                     e.response.close();
                 }
             } else {
-                if (fileMap != null) fileMap.writeError( info + "\t" + e.error());
+                if (fileMap != null) {
+                    if (infoList == null || infoList.size() == 0)
+                        fileMap.writeError(e.error());
+                    else
+                        fileMap.writeError(String.join("\n", infoList.stream()
+                                .map(line -> line + "\t" + e.error())
+                                .collect(Collectors.toList())));
+                }
             }
         }
     }
@@ -48,8 +64,7 @@ public class HttpResponseUtils {
         if (response == null) return null;
         String responseBody = response.bodyString();
         int statusCode = response.statusCode;
-        if ((statusCode == -1) || (statusCode >= 300 && statusCode != 579))
-            throw new QiniuException(response);
+        if (statusCode != 200 && statusCode != 298) throw new QiniuException(response);
         response.close();
         return responseBody;
     }
