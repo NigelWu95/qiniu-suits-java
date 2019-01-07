3d53dd40e147b582cb517657477a5100a863c5a5
wubingheng <wubinghengajw@outlook.com>
2019-01-08 02:42:50 +0800

    update result index.

--- src/main/java/com/qiniu/service/qoss/OperationBase.java
+++ src/main/java/com/qiniu/service/qoss/OperationBase.java
@@ -68,7 +68,7 @@ public abstract class OperationBase implements ILineProcess<Map<String, String>>
         OperationBase operationBase = (OperationBase)super.clone();
         operationBase.bucketManager = new BucketManager(auth, configuration);
         operationBase.batchOperations = new BatchOperations();
-        operationBase.fileMap = new FileMap(resultPath, processName, resultTag + String.valueOf(resultIndex++));
+        operationBase.fileMap = new FileMap(resultPath, processName, resultTag + String.valueOf(++resultIndex));
         try {
             operationBase.fileMap.initDefaultWriters();
         } catch (IOException e) {
