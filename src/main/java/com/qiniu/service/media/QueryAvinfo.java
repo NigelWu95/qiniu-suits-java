e55301937ac9b11c539593fc50efccdf0d865b2a
wubingheng <wubinghengajw@outlook.com>
2019-01-07 23:05:28 +0800

    update query.

--- src/main/java/com/qiniu/service/media/QueryAvinfo.java
+++ src/main/java/com/qiniu/service/media/QueryAvinfo.java
@@ -101,7 +101,7 @@ public class QueryAvinfo implements ILineProcess<Map<String, String>>, Cloneable
             try {
                 url = urlIndex != null ? line.get(urlIndex) : protocol + "://" + domain + "/" + line.get("key");
                 String avinfo = singleWithRetry(url, retryCount);
-                if (avinfo != null) resultList.add(url + "\t" + avinfo);
+                if (avinfo != null) resultList.add(line.get("key") + "\t" + avinfo);
                 else fileMap.writeError( String.valueOf(line) + "\tpfop avinfo");
             } catch (QiniuException e) {
                 HttpResponseUtils.processException(e, fileMap, new ArrayList<String>(){{add(String.valueOf(line));}});
