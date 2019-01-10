3d5a2b0fc56ae74aa36f19cd04554f68ad468837
wubingheng <wubinghengajw@outlook.com>
2019-01-10 17:58:52 +0800

    update ITypeConvert and implements.

--- src/main/java/com/qiniu/service/convert/LineToInfoMap.java
+++ src/main/java/com/qiniu/service/convert/LineToInfoMap.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 public class LineToInfoMap implements ITypeConvert<String, Map<String, String>> {
 
     private ILineParser lineParser;
-    volatile private List<String> errorList = new ArrayList<>();
+    private List<String> errorList = new ArrayList<>();
 
     public LineToInfoMap(String parseType, String separator, Map<String, String> infoIndexMap) throws IOException {
         if (infoIndexMap == null || infoIndexMap.size() == 0) throw new IOException("there are no indexes be set.");
@@ -25,12 +25,14 @@ public class LineToInfoMap implements ITypeConvert<String, Map<String, String>> 
 
     public List<Map<String, String>> convertToVList(List<String> srcList) {
         if (srcList == null || srcList.size() == 0) return new ArrayList<>();
-        return srcList.parallelStream()
+        // 使用 parallelStream 时，添加错误行至 errorList 需要同步代码块，stream 时可以直接 errorList.add();
+        return srcList.stream()
                 .map(line -> {
                     try {
                         return lineParser.getItemMap(line);
                     } catch (Exception e) {
-                        errorList.add(line + "\t" + e.getMessage());
+//                        errorList.add(line + "\t" + e.getMessage());
+                        addError(line + "\t" + e.getMessage());
                         return null;
                     }
                 })
@@ -38,7 +40,19 @@ public class LineToInfoMap implements ITypeConvert<String, Map<String, String>> 
                 .collect(Collectors.toList());
     }
 
+    synchronized private void addError(String errorLine) {
+        errorList.add(errorLine);
+    }
+
     public List<String> getErrorList() {
         return errorList;
+    }
+
+    public List<String> consumeErrorList() {
+        List<String> errors = new ArrayList<>();
+        Collections.addAll(errors, new String[errorList.size()]);
+        Collections.copy(errors, errorList);
+        errorList.clear();
+        return errors;
     }
 }
--- src/main/java/com/qiniu/service/interfaces/ITypeConvert.java
+++ src/main/java/com/qiniu/service/interfaces/ITypeConvert.java
@@ -6,5 +6,9 @@ public interface ITypeConvert<T, V> {
 
     List<V> convertToVList(List<T> srcList);
 
+    // 获得完整的 errorList，可以多次调用
     List<String> getErrorList();
+
+    // 消费者方法，调用后返回列表并清空原列表中
+    List<String> consumeErrorList();
 }
