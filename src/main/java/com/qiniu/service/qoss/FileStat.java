071feda7c9363b2312b8a945c042383a5e46e8c7
wubingheng <wubinghengajw@outlook.com>
2019-01-15 14:22:49 +0800

    update FileStat.

--- src/main/java/com/qiniu/service/qoss/FileStat.java
+++ src/main/java/com/qiniu/service/qoss/FileStat.java
@@ -2,7 +2,6 @@ package com.qiniu.service.qoss;
 
 import com.google.gson.*;
 import com.qiniu.common.QiniuException;
-import com.qiniu.http.Response;
 import com.qiniu.service.interfaces.IStringFormat;
 import com.qiniu.service.line.FileInfoTableFormatter;
 import com.qiniu.storage.BucketManager.*;
@@ -10,14 +9,11 @@ import com.qiniu.service.interfaces.ILineProcess;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.model.FileInfo;
 import com.qiniu.util.Auth;
-import com.qiniu.util.HttpResponseUtils;
 import com.qiniu.util.JsonConvertUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class FileStat extends OperationBase implements ILineProcess<Map<String, String>>, Cloneable {
 
@@ -64,31 +60,6 @@ public class FileStat extends OperationBase implements ILineProcess<Map<String, 
         return batchOperations;
     }
 
-    public void singleRun(List<Map<String, String>> fileInfoList, int retryCount) throws QiniuException {
-        FileInfo fileInfo = null;
-        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        for (Map<String, String> line : fileInfoList) {
-            int count = retryCount;
-            try {
-                while (count > 0) {
-                    try {
-                        fileInfo = bucketManager.stat(bucket, line.get("key"));
-                        count = 0;
-                    } catch (QiniuException e) {
-                        count = HttpResponseUtils.getNextRetryCount(e, count);
-                    }
-                }
-                if (fileInfo != null) {
-                    fileInfo.key = line.get("key");
-                    if ("table".equals(format)) fileMap.writeSuccess(stringFormatter.toFormatString(fileInfo));
-                    else fileMap.writeSuccess(gson.toJson(fileInfo).replace("\\\\", "\\"));
-                } else fileMap.writeError(line.get("key") + "\tempty stat result");
-            } catch (QiniuException e) {
-                HttpResponseUtils.processException(e, fileMap, new ArrayList<String>(){{add(line.get("key"));}});
-            }
-        }
-    }
-
     @Override
     public void parseBatchResult(List<Map<String, String>> processList, String result) throws QiniuException {
         if (result == null || "".equals(result)) throw new QiniuException(null, "not valid json.");
@@ -110,36 +81,6 @@ public class FileStat extends OperationBase implements ILineProcess<Map<String, 
                     fileMap.writeError(processList.get(j).get("key") + "\t" + jsonObject.toString());
             } else {
                 fileMap.writeError(processList.get(j).get("key") + "\tempty stat result");
-            }
-        }
-    }
-
-    public void batchRun(List<Map<String, String>> fileInfoList, int retryCount) throws QiniuException {
-        int times = fileInfoList.size()/1000 + 1;
-        List<Map<String, String>> processList;
-        Response response = null;
-        String result;
-        for (int i = 0; i < times; i++) {
-            processList = fileInfoList.subList(1000 * i, i == times - 1 ? fileInfoList.size() : 1000 * (i + 1));
-            if (processList.size() > 0) {
-                batchOperations = getOperations(processList);
-                int count = retryCount;
-                while (count > 0) {
-                    try {
-                        response = bucketManager.batch(batchOperations);
-                        count = 0;
-                    } catch (QiniuException e1) {
-                        count = HttpResponseUtils.getNextRetryCount(e1, count);
-                    }
-                }
-                batchOperations.clearOps();
-                try {
-                    result = HttpResponseUtils.getResult(response);
-                    parseBatchResult(processList, result);
-                } catch (QiniuException e) {
-                    HttpResponseUtils.processException(e, fileMap, processList.stream().map(line -> line.get("key"))
-                            .collect(Collectors.toList()));
-                }
             }
         }
     }
