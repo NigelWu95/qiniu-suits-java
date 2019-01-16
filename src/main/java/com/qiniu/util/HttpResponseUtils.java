64c8be7ec66b48f5966e791e87c9328e6e2ecaea
wubingheng <wubinghengajw@outlook.com>
2019-01-16 12:20:44 +0800

    update HttpResponseUtils.

--- src/main/java/com/qiniu/util/HttpResponseUtils.java
+++ src/main/java/com/qiniu/util/HttpResponseUtils.java
@@ -10,28 +10,37 @@ import java.util.stream.Collectors;
 
 public class HttpResponseUtils {
 
+    public static void writeLog(QiniuException e, FileMap fileMap, List<String> infoList) {
+        if (fileMap == null) return;
+        String message = "";
+        if (e != null) {
+            message = e.getMessage() == null ? "" : e.getMessage();
+            if ("".equals(message)) {
+                message = e.response != null ? e.response.reqId + "\t" : "";
+                try {
+                    message += e.error() == null ? "" : e.error();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        if (infoList == null || infoList.size() == 0) {
+            fileMap.writeError(message.replaceAll("\n", "\t"));
+        } else {
+            String finalMessage = message;
+            fileMap.writeError(String.join("\n", infoList.stream()
+                    .map(line -> line + "\t" + finalMessage.replaceAll("\n", "\t"))
+                    .collect(Collectors.toList())));
+        }
+    }
+
     public static void processException(QiniuException e, int retry, FileMap fileMap, List<String> infoList)
             throws QiniuException {
         // 取 error 信息优先从 exception 的 message 中取，避免直接调用 e.error() 抛出非预期异常，同时 getMessage 包含 reqid 等信息
         if (e != null) {
             // 没有重试机会时将错误信息记录下来
             if (retry <= 0 && fileMap != null) {
-                String message = e.getMessage() == null ? "" : e.getMessage();
-                if ("".equals(message)) {
-                    try {
-                        message = (e.response != null ? e.response.reqId + "\t" : "") + (e.error() == null ? "" : e.error());
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-                }
-                if (infoList == null || infoList.size() == 0) {
-                    fileMap.writeError(message.replaceAll("\n", "\t"));
-                } else {
-                    String finalMessage = message;
-                    fileMap.writeError(String.join("\n", infoList.stream()
-                            .map(line -> line + "\t" + finalMessage.replaceAll("\n", "\t"))
-                            .collect(Collectors.toList())));
-                }
+                writeLog(e, fileMap, infoList);
             }
             if (e.response != null) {
                 if (retry <= 0 || e.response.statusCode >= 630 || !e.response.needRetry()) {
