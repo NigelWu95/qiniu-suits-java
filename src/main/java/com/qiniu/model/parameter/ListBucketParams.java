0eeaf182499aa39788c0d74cb869b9eae9c44097
wubingheng <wubinghengajw@outlook.com>
2019-02-22 00:20:03 +0800

    for the situation of listing by multiple prefixes, add the boundary options "prefix-left" and "prefix-right", these can make you choose whether listing for items before or after custom prefixes(first and last items).

--- src/main/java/com/qiniu/model/parameter/ListBucketParams.java
+++ src/main/java/com/qiniu/model/parameter/ListBucketParams.java
@@ -9,6 +9,8 @@ public class ListBucketParams extends QossParams {
 
     private String prefixes;
     private String antiPrefixes;
+    private String prefixLeft;
+    private String prefixRight;
     private String marker;
     private String end;
 
@@ -16,6 +18,8 @@ public class ListBucketParams extends QossParams {
         super(entryParam);
         try { this.prefixes = entryParam.getParamValue("prefixes"); } catch (Exception e) { this.prefixes = ""; }
         try { this.antiPrefixes = entryParam.getParamValue("anti-prefixes"); } catch (Exception e) { this.antiPrefixes = ""; }
+        try { this.prefixLeft = entryParam.getParamValue("prefix-left"); } catch (Exception e) { this.prefixLeft = ""; }
+        try { this.prefixRight = entryParam.getParamValue("prefix-right"); } catch (Exception e) { this.prefixRight = ""; }
         try { this.marker = entryParam.getParamValue("marker"); } catch (Exception e) {}
         try { this.end = entryParam.getParamValue("end"); } catch (Exception e) {}
     }
@@ -28,19 +32,35 @@ public class ListBucketParams extends QossParams {
         return null;
     }
 
-    public String getMarker() {
-        return marker;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
     public List<String> getAntiPrefixes() {
         if (!"".equals(antiPrefixes)) {
             Set<String> set = new HashSet<>(Arrays.asList(antiPrefixes.split(",")));
             return new ArrayList<>(set);
         }
         return null;
+    }
+
+    public boolean getPrefixLeft() {
+        if (prefixLeft.matches("(true|false)")) {
+            return Boolean.valueOf(prefixLeft);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean getPrefixRight() {
+        if (prefixRight.matches("(true|false)")) {
+            return Boolean.valueOf(prefixRight);
+        } else {
+            return false;
+        }
+    }
+
+    public String getMarker() {
+        return marker;
+    }
+
+    public String getEnd() {
+        return end;
     }
 }
