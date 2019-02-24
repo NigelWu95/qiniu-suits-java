583e876ff7d26d112657629db5e6da457a5a6fee
wubingheng <wubinghengajw@outlook.com>
2019-02-24 19:37:11 +0800

    update custom entry and add custom mime check to filter the ext with mime type.

--- src/main/java/com/qiniu/custom/CustomEntry.java
+++ src/main/java/com/qiniu/custom/CustomEntry.java
@@ -10,36 +10,31 @@ import com.qiniu.service.interfaces.IEntryParam;
 import com.qiniu.service.interfaces.ILineProcess;
 import com.qiniu.storage.Configuration;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class CFopEntry {
+public class CustomEntry {
+
+    static private boolean saveTotal;
+    static private String resultFormat;
+    static private String resultSeparator;
+    static private String resultPath;
+    static private List<String> removeFields;
+    static private ILineProcess<Map<String, String>> processor;
 
     public static void main(String[] args) throws Exception {
 
         IEntryParam entryParam = new PropertyConfig("resources/.qiniu-fantx.properties");
-        HttpParams httpParams = new HttpParams(entryParam);
-        Configuration configuration = new Configuration(Zone.autoZone());
-        configuration.connectTimeout = httpParams.getConnectTimeout();
-        configuration.readTimeout = httpParams.getReadTimeout();
-        configuration.writeTimeout = httpParams.getWriteTimeout();
 
-        QossParams qossParams = new QossParams(entryParam);
-        boolean saveTotal = qossParams.getSaveTotal();
-        String resultFormat = qossParams.getResultFormat();
-        String resultSeparator = qossParams.getResultSeparator();
-        String resultPath = qossParams.getResultPath();
-        List<String> removeFields = qossParams.getRmFields();
         FileInputParams fileInputParams = new FileInputParams(entryParam);
         Map<String, String> indexMap = fileInputParams.getIndexMap();
-        ILineProcess<Map<String, String>> processor;
-        processor = new ProcessorChoice(entryParam, configuration).getFileProcessor();
         // parse avinfo from files.
         indexMap.put("2", "avinfo");
-        processor = new CAvinfoProcess(qossParams.getBucket(), resultPath);
+        setFopProcessor(entryParam);
 
-        int unitLen = qossParams.getUnitLen();
-        int threads = qossParams.getThreads();
+        int unitLen = fileInputParams.getUnitLen();
+        int threads = fileInputParams.getThreads();
         String filePath = fileInputParams.getFilePath();
         String parseType = fileInputParams.getParseType();
         String separator = fileInputParams.getSeparator();
@@ -48,5 +43,23 @@ public class CFopEntry {
         dataSource.setResultSaveOptions(saveTotal, resultFormat, resultSeparator, removeFields);
         dataSource.export(threads, processor);
         processor.closeResource();
+    }
+
+    static private void setFopProcessor(IEntryParam entryParam) throws Exception {
+
+        HttpParams httpParams = new HttpParams(entryParam);
+        Configuration configuration = new Configuration(Zone.autoZone());
+        configuration.connectTimeout = httpParams.getConnectTimeout();
+        configuration.readTimeout = httpParams.getReadTimeout();
+        configuration.writeTimeout = httpParams.getWriteTimeout();
+
+        QossParams qossParams = new QossParams(entryParam);
+        saveTotal = qossParams.getSaveTotal();
+        resultFormat = qossParams.getResultFormat();
+        resultSeparator = qossParams.getResultSeparator();
+        resultPath = qossParams.getResultPath();
+        removeFields = qossParams.getRmFields();
+        processor = new ProcessorChoice(entryParam, configuration).getFileProcessor();
+        processor = new CAvinfoProcess(qossParams.getBucket(), resultPath);
     }
 }
