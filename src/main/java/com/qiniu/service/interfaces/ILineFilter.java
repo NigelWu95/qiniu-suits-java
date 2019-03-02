ace74530f49f434598f55930a2d7b910a166364b
wubingheng <wubinghengajw@outlook.com>
2019-03-02 15:34:03 +0800

    update FilterProcess.

--- src/main/java/com/qiniu/service/interfaces/ILineFilter.java
+++ src/main/java/com/qiniu/service/interfaces/ILineFilter.java
@@ -1,10 +1,6 @@
 package com.qiniu.service.interfaces;
 
-import java.util.List;
-
 public interface ILineFilter<T> {
 
     boolean doFilter(T line) throws Exception;
-
-//    List<T> doFilter(List<T> lineList) throws Exception;
 }
