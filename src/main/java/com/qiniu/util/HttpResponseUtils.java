a7885a70ae15279628a09b91f2ee1471416d9603
wubingheng <wubinghengajw@outlook.com>
2019-03-17 23:23:29 +0800

    fix HttpResponseUtils processException method to move the retry times reduce operation to the end after judge by it.

--- src/main/java/com/qiniu/util/HttpResponseUtils.java
+++ src/main/java/com/qiniu/util/HttpResponseUtils.java
@@ -52,8 +52,6 @@ public class HttpResponseUtils {
      */
     public static int processException(QiniuException e, int retry, FileMap fileMap, List<String> infoList)
             throws IOException {
-        // 处理一次异常返回的重试次数应该少一次
-        retry--;
         // 取 error 信息优先从 exception 的 message 中取，避免直接调用 e.error() 抛出非预期异常，同时 getMessage 包含 reqid 等信息
         if (e != null) {
             System.out.println(e.getMessage());
@@ -85,6 +83,8 @@ public class HttpResponseUtils {
             }
         }
 
+        // 处理一次异常返回的重试次数应该少一次
+        retry--;
         return retry;
     }
 
