ad8d7ca21af0080a78fccfe4286d94026ea7b9ea
wubingheng <wubinghengajw@outlook.com>
2019-03-23 23:07:49 +0800

    update Avinfo process.

--- src/main/java/com/qiniu/process/qdora/Avinfo.java
+++ src/main/java/com/qiniu/process/qdora/Avinfo.java
@@ -1,28 +1,30 @@
-package com.qiniu.process.qoss;
+package com.qiniu.process.qdora;
 
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
+import com.qiniu.interfaces.ILineProcess;
+import com.qiniu.persistence.FileMap;
 import com.qiniu.process.Base;
 import com.qiniu.util.*;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class QHash extends Base {
+public class Avinfo extends Base {
 
     private String domain;
     private String protocol;
     final private String urlIndex;
-    final private String algorithm;
-    private FileChecker fileChecker;
+    private MediaManager mediaManager;
     private JsonParser jsonParser;
 
-    public QHash(String domain, String algorithm, String protocol, String urlIndex, String accessKey, String secretKey,
-                 String rmPrefix, String savePath, int saveIndex) throws IOException {
-        super("qhash", accessKey, secretKey, null, null, rmPrefix, savePath, saveIndex);
+    public Avinfo(String domain, String protocol, String urlIndex, String accessKey, String secretKey,
+                  String rmPrefix, String savePath, int saveIndex) throws IOException {
+        super("avinfo", accessKey, secretKey, null, null, rmPrefix, savePath, saveIndex);
         if (urlIndex == null || "".equals(urlIndex)) {
             this.urlIndex = null;
             if (domain == null || "".equals(domain)) throw new IOException("please set one of domain and urlIndex.");
@@ -32,23 +34,34 @@ public class QHash extends Base {
                 this.protocol = protocol == null || !protocol.matches("(http|https)") ? "http" : protocol;
             }
         } else this.urlIndex = urlIndex;
-        this.algorithm = algorithm;
-        this.fileChecker = new FileChecker(algorithm, protocol, accessKey == null ? null :
+        this.mediaManager = new MediaManager(protocol, accessKey == null ? null :
                 Auth.create(accessKey, secretKey));
         this.jsonParser = new JsonParser();
     }
 
-    public QHash(String domain, String algorithm, String protocol, String urlIndex, String accessKey, String secretKey,
-                 String rmPrefix, String savePath) throws IOException {
-        this(domain, algorithm, protocol, urlIndex, accessKey, secretKey, rmPrefix, savePath, 0);
+    public Avinfo(String domain, String protocol, String urlIndex, String accessKey, String secretKey,
+                  String rmPrefix, String savePath) throws IOException {
+        this(domain, protocol, urlIndex, accessKey, secretKey, rmPrefix, savePath, 0);
     }
 
-    public QHash clone() throws CloneNotSupportedException {
-        QHash qHash = (QHash)super.clone();
-        qHash.fileChecker = new FileChecker(algorithm, protocol, accessKey == null ? null :
+    public String getProcessName() {
+        return this.processName;
+    }
+
+    public void setRetryTimes(int retryTimes) {
+        this.retryTimes = retryTimes < 1 ? 3 : retryTimes;
+    }
+
+    public void setSaveTag(String saveTag) {
+        this.saveTag = saveTag == null ? "" : saveTag;
+    }
+
+    public Avinfo clone() throws CloneNotSupportedException {
+        Avinfo avinfo = (Avinfo)super.clone();
+        avinfo.mediaManager = new MediaManager(protocol, accessKey == null ? null :
                 Auth.create(accessKey, secretKey));
-        qHash.jsonParser = new JsonParser();
-        return qHash;
+        avinfo.jsonParser = new JsonParser();
+        return avinfo;
     }
 
     protected Map<String, String> formatLine(Map<String, String> line) throws IOException {
@@ -63,7 +76,7 @@ public class QHash extends Base {
     }
 
     protected String resultInfo(Map<String, String> line) {
-        return line.get("key") + "\t" + line.get("url");
+        return line.get("key") + "\t" + line.get(urlIndex);
     }
 
     protected Response batchResult(List<Map<String, String>> lineList) {
@@ -71,11 +84,11 @@ public class QHash extends Base {
     }
 
     protected String singleResult(Map<String, String> line) throws QiniuException {
-        String qhash = fileChecker.getQHashBody(line.get("url"));
-        if (qhash != null && !"".equals(qhash)) {
+        String avinfo = mediaManager.getAvinfoBody(line.get("url"));
+        if (avinfo != null && !"".equals(avinfo)) {
             // 由于响应的 body 经过格式化通过 JsonParser 处理为一行字符串
             try {
-                return jsonParser.parse(qhash).toString();
+                return jsonParser.parse(avinfo).toString();
             } catch (JsonParseException e) {
                 throw new QiniuException(e);
             }
