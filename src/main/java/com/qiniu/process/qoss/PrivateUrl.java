28ac1e9ad5b3b06b14b39e5c445c08feabb267c9
wubingheng <wubinghengajw@outlook.com>
2019-03-24 13:38:25 +0800

    update PrivateUrl to extends Base.

--- src/main/java/com/qiniu/process/qoss/PrivateUrl.java
+++ src/main/java/com/qiniu/process/qoss/PrivateUrl.java
@@ -1,7 +1,7 @@
 package com.qiniu.process.qoss;
 
-import com.qiniu.persistence.FileMap;
-import com.qiniu.interfaces.ILineProcess;
+import com.qiniu.http.Response;
+import com.qiniu.process.Base;
 import com.qiniu.util.Auth;
 import com.qiniu.util.FileNameUtils;
 import com.qiniu.util.RequestUtils;
@@ -11,28 +11,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class PrivateUrl implements ILineProcess<Map<String, String>>, Cloneable {
+public class PrivateUrl extends Base {
 
     private String domain;
     private String protocol;
-    final private String urlIndex;
-    final private String accessKey;
-    final private String secretKey;
+    private String urlIndex;
+    private long expires;
     private Auth auth;
-    final private long expires;
-    final private String processName;
-    final private String rmPrefix;
-    final private String savePath;
-    private String saveTag;
-    private int saveIndex;
-    private FileMap fileMap;
 
     public PrivateUrl(String accessKey, String secretKey, String domain, String protocol, String urlIndex, long expires,
                       String rmPrefix, String savePath, int saveIndex) throws IOException {
-        this.processName = "privateurl";
-        this.accessKey = accessKey;
-        this.secretKey = secretKey;
-        this.auth = Auth.create(accessKey, secretKey);
+        super("privateurl", accessKey, secretKey, null, null, rmPrefix, savePath, saveIndex);
         if (urlIndex == null || "".equals(urlIndex)) {
             this.urlIndex = null;
             if (domain == null || "".equals(domain)) throw new IOException("please set one of domain and urlIndex.");
@@ -43,12 +32,7 @@ public class PrivateUrl implements ILineProcess<Map<String, String>>, Cloneable 
             }
         } else this.urlIndex = urlIndex;
         this.expires = expires == 0L ? 3600 : expires;
-        this.rmPrefix = rmPrefix;
-        this.savePath = savePath;
-        this.saveTag = "";
-        this.saveIndex = saveIndex;
-        this.fileMap = new FileMap(savePath, processName, String.valueOf(saveIndex));
-        this.fileMap.initDefaultWriters();
+        this.auth = Auth.create(accessKey, secretKey);
     }
 
     public PrivateUrl(String accessKey, String secretKey, String domain, String protocol, String urlIndex, long expires,
@@ -56,50 +40,35 @@ public class PrivateUrl implements ILineProcess<Map<String, String>>, Cloneable 
         this(accessKey, secretKey, domain, protocol, urlIndex, expires, rmPrefix, savePath, 0);
     }
 
-    public String getProcessName() {
-        return this.processName;
-    }
-
-    public void setSaveTag(String saveTag) {
-        this.saveTag = saveTag == null ? "" : saveTag;
-    }
-
     public PrivateUrl clone() throws CloneNotSupportedException {
         PrivateUrl privateUrl = (PrivateUrl)super.clone();
         privateUrl.auth = Auth.create(accessKey, secretKey);
-        privateUrl.fileMap = new FileMap(savePath, processName, saveTag + String.valueOf(++saveIndex));
-        try {
-            privateUrl.fileMap.initDefaultWriters();
-        } catch (IOException e) {
-            throw new CloneNotSupportedException("init writer failed.");
-        }
         return privateUrl;
     }
 
-    public void processLine(List<Map<String, String>> lineList) throws IOException {
-        String url;
-        String key;
-        String signedUrl;
-        for (Map<String, String> line : lineList) {
-            try {
-                if (urlIndex != null) {
-                    url = line.get(urlIndex);
-                    key = URLUtils.getKey(url);
-                } else  {
-                    key = FileNameUtils.rmPrefix(rmPrefix, line.get("key")).replaceAll("\\?", "%3F");
-                    url = protocol + "://" + domain + "/" + key;
-                }
-            } catch (IOException e) {
-                fileMap.writeError(String.valueOf(line) + "\t" + e.getMessage(), false);
-                continue;
-            }
-            String finalInfo = key + "\t" + url;
-            signedUrl = auth.privateDownloadUrl(url, expires);
-            fileMap.writeSuccess(finalInfo + "\t" + signedUrl, false);
+    @Override
+    protected Map<String, String> formatLine(Map<String, String> line) throws IOException {
+        if (urlIndex != null) {
+            line.put("key", URLUtils.getKey(line.get(urlIndex)));
+        } else  {
+            line.put("key", FileNameUtils.rmPrefix(rmPrefix, line.get("key"))
+                    .replaceAll("\\?", "%3F"));
+            urlIndex = "url";
+            line.put(urlIndex, protocol + "://" + domain + "/" + line.get("key"));
         }
+        return line;
     }
 
-    public void closeResource() {
-        fileMap.closeWriters();
+    @Override
+    protected String resultInfo(Map<String, String> line) {
+        return line.get("key") + "\t" + line.get(urlIndex);
+    }
+
+    protected Response batchResult(List<Map<String, String>> lineList) {
+        return null;
+    }
+
+    protected String singleResult(Map<String, String> line) {
+        return auth.privateDownloadUrl(line.get(urlIndex), expires);
     }
 }
