81ef759757d355c5641af5100ff07118c7bc9531
wubingheng <wubinghengajw@outlook.com>
2019-03-24 12:06:40 +0800

    update PfopCommand to extends Base.

--- src/main/java/com/qiniu/process/qdora/PfopCommand.java
+++ src/main/java/com/qiniu/process/qdora/PfopCommand.java
@@ -1,13 +1,15 @@
 package com.qiniu.process.qdora;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import com.qiniu.common.QiniuException;
 import com.qiniu.config.JsonFile;
+import com.qiniu.http.Response;
 import com.qiniu.model.qdora.Avinfo;
 import com.qiniu.model.qdora.VideoStream;
-import com.qiniu.persistence.FileMap;
-import com.qiniu.interfaces.ILineProcess;
+import com.qiniu.process.Base;
 import com.qiniu.util.FileNameUtils;
 import com.qiniu.util.JsonConvertUtils;
 import com.qiniu.util.UrlSafeBase64;
@@ -17,24 +19,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class PfopCommand implements ILineProcess<Map<String, String>>, Cloneable {
+public class PfopCommand extends Base {
 
-    final private String processName;
     private MediaManager mediaManager;
-    private FileMap fileMap;
     private boolean hasDuration;
     private boolean hasSize;
     private String avinfoIndex;
     private ArrayList<JsonObject> pfopConfigs;
-    final private String rmPrefix;
-    private String savePath;
-    private String saveTag;
-    private int saveIndex;
+    private Gson gson;
 
     public PfopCommand(String jsonPath, boolean hasDuration, boolean hasSize, String avinfoIndex, String rmPrefix,
                        String savePath, int saveIndex) throws IOException {
-        this.processName = "pfopcmd";
-        pfopConfigs = new ArrayList<>();
+        super("pfopcmd", null, null, null, null, rmPrefix, savePath, saveIndex);
+        this.pfopConfigs = new ArrayList<>();
         JsonFile jsonFile = new JsonFile(jsonPath);
         for (String key : jsonFile.getConfigKeys()) {
             JsonObject jsonObject = jsonFile.getElement(key).getAsJsonObject();
@@ -53,19 +50,14 @@ public class PfopCommand implements ILineProcess<Map<String, String>>, Cloneable
             else if (!jsonObject.get("saveas").getAsString().contains(":"))
                 throw new IOException(jsonPath + " miss the <bucket> field of \"saveas\" field in \"" + key + "\"");
             jsonObject.addProperty("name", key);
-            pfopConfigs.add(jsonObject);
+            this.pfopConfigs.add(jsonObject);
         }
         this.mediaManager = new MediaManager();
         this.hasDuration = hasDuration;
         this.hasSize = hasSize;
         if (avinfoIndex == null || "".equals(avinfoIndex)) throw new IOException("please set the avinfoIndex.");
         else this.avinfoIndex = avinfoIndex;
-        this.rmPrefix = rmPrefix;
-        this.savePath = savePath;
-        this.saveTag = "";
-        this.saveIndex = saveIndex;
-        this.fileMap = new FileMap(savePath, processName, String.valueOf(saveIndex));
-        this.fileMap.initDefaultWriters();
+        this.gson = new Gson();
     }
 
     public PfopCommand(String jsonPath, boolean hasDuration, boolean hasSize, String avinfoIndex, String rmPrefix,
@@ -73,26 +65,56 @@ public class PfopCommand implements ILineProcess<Map<String, String>>, Cloneable
         this(jsonPath, hasDuration, hasSize, avinfoIndex, rmPrefix, savePath, 0);
     }
 
-    public String getProcessName() {
-        return this.processName;
-    }
-
-    public void setSaveTag(String saveTag) {
-        this.saveTag = saveTag == null ? "" : saveTag;
-    }
-
     @SuppressWarnings("unchecked")
     public PfopCommand clone() throws CloneNotSupportedException {
         PfopCommand pfopCommand = (PfopCommand)super.clone();
         pfopCommand.mediaManager = new MediaManager();
         pfopCommand.pfopConfigs = (ArrayList<JsonObject>) pfopConfigs.clone();
-        pfopCommand.fileMap = new FileMap(savePath, processName, saveTag + String.valueOf(++saveIndex));
-        try {
-            pfopCommand.fileMap.initDefaultWriters();
-        } catch (IOException e) {
-            throw new CloneNotSupportedException("init writer failed.");
-        }
+        pfopCommand.gson = new Gson();
         return pfopCommand;
+    }
+
+    protected Map<String, String> formatLine(Map<String, String> line) {
+        return line;
+    }
+
+    protected String resultInfo(Map<String, String> line) {
+        return line.get("key") + "\t" + line.get(avinfoIndex);
+    }
+
+    protected Response batchResult(List<Map<String, String>> lineList) {
+        return null;
+    }
+
+    protected String singleResult(Map<String, String> line) throws QiniuException {
+        String key;
+        String info;
+        Avinfo avinfo;
+        StringBuilder other = new StringBuilder();
+        VideoStream videoStream;
+        List<Integer> scale;
+        List<String> items = new ArrayList<>();
+        for (JsonObject pfopConfig : pfopConfigs) {
+            scale = gson.fromJson(pfopConfig.get("scale").getAsJsonArray(), new TypeToken<List<Integer>>(){}.getType());
+            key = line.get("key");
+            info = line.get(avinfoIndex);
+            try {
+                if (key == null || "".equals(key) || info == null || "".equals(info))
+                    throw new IOException("key or avinfo is empty.");
+                key = FileNameUtils.rmPrefix(rmPrefix, line.get("key"));
+                avinfo = mediaManager.getAvinfoByJson(info);
+                if (hasDuration) other.append("\t").append(Double.valueOf(avinfo.getFormat().duration));
+                if (hasSize) other.append("\t").append(Long.valueOf(avinfo.getFormat().size));
+                videoStream = avinfo.getVideoStream();
+                if (videoStream == null) throw new Exception("videoStream is null");
+                if (scale.get(0) < videoStream.width && videoStream.width <= scale.get(1)) {
+                    items.add(key + "\t" + generateFopCmd(key, pfopConfig) + other.toString());
+                }
+            } catch (Exception e) {
+                throw new QiniuException(e);
+            }
+        }
+        return String.join("\n", items);
     }
 
     private String generateFopCmd(String srcKey, JsonObject pfopJson) throws IOException {
@@ -109,41 +131,5 @@ public class PfopCommand implements ILineProcess<Map<String, String>>, Cloneable
             saveAs = saveAs.replace(saveAs.substring(saveAs.indexOf(":") + 1), saveAsKey);
         }
         return pfopJson.get("cmd").getAsString() + "|saveas/" + UrlSafeBase64.encodeToString(saveAs);
-    }
-
-    public void processLine(List<Map<String, String>> lineList) throws IOException {
-        String key;
-        String info;
-        Avinfo avinfo;
-        StringBuilder other = new StringBuilder();
-        VideoStream videoStream;
-        List<Integer> scale;
-        for (JsonObject pfopConfig : pfopConfigs) {
-            scale = JsonConvertUtils.fromJsonArray(pfopConfig.get("scale").getAsJsonArray(),
-                    new TypeToken<List<Integer>>(){});
-            for (Map<String, String> line : lineList) {
-                key = line.get("key");
-                info = line.get(avinfoIndex);
-                try {
-                    if (key == null || "".equals(key) || info == null || "".equals(info))
-                        throw new IOException("key or avinfo is empty.");
-                    key = FileNameUtils.rmPrefix(rmPrefix, line.get("key"));
-                    avinfo = mediaManager.getAvinfoByJson(info);
-                    if (hasDuration) other.append("\t").append(Double.valueOf(avinfo.getFormat().duration));
-                    if (hasSize) other.append("\t").append(Long.valueOf(avinfo.getFormat().size));
-                    videoStream = avinfo.getVideoStream();
-                    if (videoStream == null) throw new Exception("videoStream is null");
-                    if (scale.get(0) < videoStream.width && videoStream.width <= scale.get(1)) {
-                        fileMap.writeSuccess(key + "\t" + generateFopCmd(key, pfopConfig) + other.toString(), false);
-                    }
-                } catch (Exception e) {
-                    fileMap.writeError(String.valueOf(line) + "\t" + e.getMessage(), false);
-                }
-            }
-        }
-    }
-
-    public void closeResource() {
-        fileMap.closeWriters();
     }
 }
