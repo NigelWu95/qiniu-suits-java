60823ac72c55bf0fcd4da810b35b443862c4ed0e
wubingheng <wubinghengajw@outlook.com>
2019-03-31 13:50:56 +0800

    add M3U8Manager to process ts in m3u8 and add ExportTs process.

--- src/main/java/com/qiniu/util/M3U8Utils.java
+++ src/main/java/com/qiniu/util/M3U8Utils.java
@@ -1,5 +1,6 @@
 package com.qiniu.util;
 
+import com.qiniu.model.qdora.VideoTS;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
@@ -8,38 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class M3U8Utils {
-
-    public static class VideoTS {
-
-        private String url;
-        private float seconds;
-
-        public VideoTS(String url, float seconds) {
-            this.url = url;
-            this.seconds = seconds;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public float getSeconds() {
-            return seconds;
-        }
-
-        public void setSeconds(float seconds) {
-            this.seconds = seconds;
-        }
-
-        public String toString() {
-            return url + ": " + seconds + "sec";
-        }
-
-    }
 
     public static void merge(List<VideoTS> tsList, String url, String targetFileDir) throws IOException {
         File fileDir = new File(targetFileDir);
@@ -104,55 +73,5 @@ public class M3U8Utils {
         } catch (IOException e) {
             inputStream = null;
         }
-    }
-
-    public static List<VideoTS> getVideoTSListByUrl(String m3u8Url) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(m3u8Url).openStream()));
-        String rootUrl = m3u8Url.substring(0, m3u8Url.indexOf("/", 8) + 1);
-        List<VideoTS> ret = getVideoTSList(bufferedReader, rootUrl);
-        bufferedReader.close();
-
-        return ret;
-    }
-
-    public static List<VideoTS> getVideoTSListByFile(String rootUrl, String m3u8FilePath) throws IOException {
-
-        FileReader fileReader = new FileReader(new File(m3u8FilePath));
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        List<VideoTS> ret = getVideoTSList(bufferedReader, rootUrl);
-        bufferedReader.close();
-
-        return ret;
-    }
-
-    public static List<VideoTS> getVideoTSList(BufferedReader bufferedReader, String rootUrl) throws IOException {
-
-        List<VideoTS> ret = new ArrayList<>();
-        String line;
-        float seconds = 0;
-
-        while ((line = bufferedReader.readLine()) != null) {
-            if (line.startsWith("#")) {
-                if (line.startsWith("#EXTINF:")) {
-                    line = line.substring(8, line.indexOf(","));
-                    seconds = Float.parseFloat(line);
-                }
-                continue;
-            }
-
-            String url = line.startsWith("http") ? line : line.startsWith("/") ? rootUrl + line.substring(1) :
-                    rootUrl + line;
-            if (line.endsWith(".m3u8")) {
-                List<VideoTS> tsList = getVideoTSListByUrl(url);
-                ret.addAll(tsList);
-            } else {
-                ret.add(new VideoTS(url, seconds));
-            }
-
-            seconds = 0;
-        }
-
-        return ret;
     }
 }
