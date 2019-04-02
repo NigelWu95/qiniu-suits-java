9f5f3b74939133f7737d98f1ace7d466d0b3a77d
wubingheng <wubinghengajw@outlook.com>
2019-04-02 10:06:11 +0800

    update ts url get.

--- src/main/java/com/qiniu/process/qoss/M3U8Manager.java
+++ src/main/java/com/qiniu/process/qoss/M3U8Manager.java
@@ -52,7 +52,7 @@ public class M3U8Manager {
             }
 
             String url = line.startsWith("http") ? line : protocol + "://" +
-                    (line.startsWith("/") ? domain + "/" + line.substring(1) : domain + "/" + line);
+                    (line.startsWith("/") ? domain + line : domain + "/" + line);
             if (line.endsWith(".m3u8")) {
                 List<VideoTS> tsList = getVideoTSListByUrl(url);
                 ret.addAll(tsList);
