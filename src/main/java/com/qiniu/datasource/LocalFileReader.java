a3837b57b2c33f64ef82ea6c6c305e3ec00a200f
wubingheng <wubinghengajw@outlook.com>
2019-04-14 00:46:40 +0800

    add IReader implements named LocalFileReader and use paradigm to set the real reader type.

--- src/main/java/com/qiniu/datasource/LocalFileReader.java
+++ src/main/java/com/qiniu/datasource/LocalFileReader.java
@@ -1,25 +1,48 @@
 package com.qiniu.datasource;
 
-import java.io.BufferedReader;
+import com.qiniu.util.FileNameUtils;
 
-public class LocalFileReader implements IReader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class LocalFileReader implements IReader<BufferedReader> {
 
     private String name;
     private BufferedReader bufferedReader;
+
+    public LocalFileReader(String filepath) throws IOException {
+        if (filepath.endsWith(".txt")) {
+            filepath = FileNameUtils.realPathWithUserHome(filepath);
+            File sourceFile = new File(filepath);
+            FileReader fileReader;
+            try {
+                fileReader = new FileReader(sourceFile);
+            } catch (IOException e) {
+                throw new IOException("file-path parameter may be incorrect, " + e.getMessage());
+            }
+            bufferedReader = new BufferedReader(fileReader);
+            name = sourceFile.getName().substring(0, sourceFile.getName().length() - 4);
+        } else {
+            throw new IOException("please provide the .txt file. The current path you gave is: " + filepath);
+        }
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BufferedReader getBufferedReader() {
+    public BufferedReader getRealReader() {
         return bufferedReader;
     }
 
-    public void setBufferedReader(BufferedReader bufferedReader) {
-        this.bufferedReader = bufferedReader;
+    public String read() throws IOException {
+        return bufferedReader.readLine();
+    }
+
+    public Stream<String> lines() {
+        return bufferedReader.lines();
     }
 }
