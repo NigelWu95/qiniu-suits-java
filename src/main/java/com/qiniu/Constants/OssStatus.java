a2c0612ff57fbdb89fc79ec556e33eac3e255d27
wubingheng <wubinghengajw@outlook.com>
2019-04-17 16:51:58 +0800

    add OssStatus to process ali oss sdk's statusCode.

---
+++ src/main/java/com/qiniu/Constants/OssStatus.java
@@ -0,0 +1,40 @@
+package com.qiniu.Constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OssStatus {
+
+    public static Map<String, Integer> aliMap = new HashMap<String, Integer>(){{
+        put("AccessDenied", 403);// 拒绝访问
+        put("BucketAlreadyExists", 409);// 存储空间已经存在
+        put("BucketNotEmpty", 409);// 存储空间非空
+        put("EntityTooLarge", 400);// 实体过大
+        put("EntityTooSmall", 400);// 实体过小
+        put("FileGroupTooLarge", 400);// 文件组过大
+        put("FilePartNotExist", 400);// 文件分片不存在
+        put("FilePartStale", 400);// 文件分片过时
+        put("InvalidArgument", 400);// 参数格式错误
+        put("InvalidAccessKeyId", 403);// AccessKeyId不存在
+        put("InvalidBucketName", 400);// 无效的存储空间名称
+        put("InvalidDigest", 400);// 无效的摘要
+        put("InvalidObjectName", 400);// 无效的文件名称
+        put("InvalidPart", 400);// 无效的分片
+        put("InvalidPartOrder", 400);// 无效的分片顺序
+        put("InvalidTargetBucketForLogging", 400);// Logging操作中有无效的目标存储空间
+        put("InternalError", 500);// OSS内部错误
+        put("MalformedXML", 400);// XML格式非法
+        put("MethodNotAllowed", 405);// 不支持的方法
+        put("MissingArgument", 411);// 缺少参数
+        put("MissingContentLength", 411);// 缺少内容长度
+        put("NoSuchBucket", 404);// 存储空间不存在
+        put("NoSuchKey", 404);// 文件不存在
+        put("NoSuchUpload", 404);// 分片上传ID不存在
+        put("NotImplemented", 501);// 无法处理的方法
+        put("PreconditionFailed", 412);// 预处理错误
+        put("RequestTimeTooSkewed", 403);// 客户端本地时间和OSS服务器时间相差超过15分钟
+        put("RequestTimeout", 400);// 请求超时
+        put("SignatureDoesNotMatch", 403);// 签名错误
+        put("InvalidEncryptionAlgorithmError", 400);// 指定的熵编码加密算法错误
+    }};
+}
