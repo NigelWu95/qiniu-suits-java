d4a49a7803db00129469bdd16e269ae3e6cc8491
wubingheng <wubinghengajw@outlook.com>
2019-04-29 22:21:55 +0800

    update ParamsConfig to be compatible with regex format string.

--- src/main/test/com/qiniu/config/ParamsConfigTest.java
+++ src/main/test/com/qiniu/config/ParamsConfigTest.java
@@ -10,7 +10,7 @@ public class ParamsConfigTest {
 
     @Test
     public void testGetValueByCommandArgs() throws IOException {
-        String[] args = new String[]{"list", "-a=\"\"", "-ak=\"1\"", "-sk=1", "-bucket=1", "-multi=1", "-max-threads=1", "-f-regex=\"(.*)\\.csv\""};
+        String[] args = new String[]{"list", "-a=\"\"", "-ak=\"1\"", "-sk=1", "-bucket=1", "-multi=1", "-max-threads=1", "-f-regex=9xiu/\\d*\\.jpg"};
         paramsConfig = new ParamsConfig(args);
         System.out.println(paramsConfig.getValue("ak"));
         System.out.println(paramsConfig.getValue("ab", "ab"));
