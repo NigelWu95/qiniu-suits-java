48bb7f4a7cdb762e4b77f249fed0b9e65aec3ddd
wubingheng <wubinghengajw@outlook.com>
2019-04-29 21:27:47 +0800

    add Converter to update type conversion.

--- src/main/java/com/qiniu/convert/MapToString.java
+++ src/main/java/com/qiniu/convert/MapToString.java
@@ -5,7 +5,7 @@ import com.qiniu.util.LineUtils;
 import java.io.IOException;
 import java.util.*;
 
-public class MapToString extends ObjectToString<Map<String, String>> {
+public class MapToString extends Converter<Map<String, String>, String> {
 
     public MapToString(String format, String separator, List<String> rmFields) throws IOException {
         // 将 file info 的字段逐一进行获取是为了控制输出字段的顺序
@@ -18,5 +18,10 @@ public class MapToString extends ObjectToString<Map<String, String>> {
         } else {
             throw new IOException("please check your format for map to string.");
         }
+    }
+
+    @Override
+    public String convertToV(Map<String, String> line) throws IOException {
+        return stringFormatter.toFormatString(line);
     }
 }
--- src/main/java/com/qiniu/convert/OSSObjToString.java
+++ src/main/java/com/qiniu/convert/OSSObjToString.java
@@ -5,8 +5,9 @@ import com.qiniu.util.LineUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-public class OSSObjToString extends ObjectToString<OSSObjectSummary> {
+public class OSSObjToString extends Converter<OSSObjectSummary, String> {
 
     public OSSObjToString(String format, String separator, List<String> rmFields) throws IOException {
         // 将 file info 的字段逐一进行获取是为了控制输出字段的顺序
@@ -19,5 +20,10 @@ public class OSSObjToString extends ObjectToString<OSSObjectSummary> {
         } else {
             throw new IOException("please check your format for map to string.");
         }
+    }
+
+    @Override
+    public String convertToV(OSSObjectSummary line) throws IOException {
+        return stringFormatter.toFormatString(line);
     }
 }
