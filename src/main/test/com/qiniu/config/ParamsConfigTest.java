2c4b1528bffa37bfabb7ac9adc4f08eef4bf6c9c
wubingheng <wubinghengajw@outlook.com>
2019-05-07 23:53:41 +0800

    update properties config test.

--- src/main/test/com/qiniu/config/ParamsConfigTest.java
+++ src/main/test/com/qiniu/config/ParamsConfigTest.java
@@ -22,10 +22,23 @@ public class ParamsConfigTest {
         paramsConfig = new ParamsConfig("resources" + System.getProperty("file.separator") + ".application.config");
         try {
             System.out.println(paramsConfig.getValue("process"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
             System.out.println(paramsConfig.getValue("no"));
-            System.out.println(paramsConfig.getValue("no", "no"));
-            System.out.println(paramsConfig.getValue("use-https", "true"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(paramsConfig.getValue("no", "no"));
+        System.out.println(paramsConfig.getValue("use-https", "true"));
+        try {
             System.out.println(paramsConfig.getValue("use-https") == null);
+            System.out.println(paramsConfig.getValue("use-https").equals(""));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
             System.out.println(paramsConfig.getValue("use-https").equals(""));
         } catch (Exception e) {
             e.printStackTrace();
