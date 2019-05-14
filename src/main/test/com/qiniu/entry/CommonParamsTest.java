40cde0ea5a5be860585b27df60e4559b3beba206
wubingheng <wubinghengajw@outlook.com>
2019-05-14 16:53:32 +0800

    optimize indexes setting with characters escape.

--- src/main/test/com/qiniu/entry/CommonParamsTest.java
+++ src/main/test/com/qiniu/entry/CommonParamsTest.java
@@ -13,7 +13,10 @@ public class CommonParamsTest {
 
     @Test
     public void test() throws Exception {
-        String[] args = new String[]{"-source=local", "-indexes=[key:key,1,2,time:time]"
+        String[] args = new String[]{"-source=local"
+                , "-indexes=[key:key,1,2,time:time]"
+                , "-indexes=[key:key,1,2,time:time,a:\\:b]"
+//                , "-indexes=[key:key,1,2,time:time,a:\\:b:c]"
                 ,"-f-prefix=fragments,abc,\\,a"
 //                ,"-f-mime=video"
                 };
