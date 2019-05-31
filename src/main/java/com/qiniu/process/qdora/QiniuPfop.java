0474eb401f03064c1251b828bb80630d7d9183e6
wubingheng <wubinghengajw@outlook.com>
2019-05-31 14:00:02 +0800

    update QiniuPfop.

--- src/main/java/com/qiniu/process/qdora/QiniuPfop.java
+++ src/main/java/com/qiniu/process/qdora/QiniuPfop.java
@@ -17,32 +17,30 @@ public class QiniuPfop extends Base<Map<String, String>> {
     private StringMap pfopParams;
     private List<JsonObject> pfopConfigs;
     private String fopsIndex;
-    private String fops;
     private OperationManager operationManager;
 
     public QiniuPfop(String accessKey, String secretKey, Configuration configuration, String bucket, String pipeline,
-                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String fops, String savePath,
+                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String savePath,
                      int saveIndex) throws IOException {
         super("pfop", accessKey, secretKey, configuration, bucket, savePath, saveIndex);
-        set(pipeline, pfopJsonPath, pfopConfigs, fopsIndex, fops);
+        set(pipeline, pfopJsonPath, pfopConfigs, fopsIndex);
         this.operationManager = new OperationManager(Auth.create(accessKey, secretKey), configuration.clone());
     }
 
     public QiniuPfop(String accessKey, String secretKey, Configuration configuration, String bucket, String pipeline,
-                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String fops) throws IOException {
+                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex) throws IOException {
         super("pfop", accessKey, secretKey, configuration, bucket);
-        set(pipeline, pfopJsonPath, pfopConfigs, fopsIndex, fops);
+        set(pipeline, pfopJsonPath, pfopConfigs, fopsIndex);
         this.operationManager = new OperationManager(Auth.create(accessKey, secretKey), configuration.clone());
     }
 
     public QiniuPfop(String accessKey, String secretKey, Configuration configuration, String bucket, String pipeline,
-                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String fops, String savePath)
+                     String pfopJsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String savePath)
             throws IOException {
-        this(accessKey, secretKey, configuration, bucket, pipeline, pfopJsonPath, pfopConfigs, fopsIndex, fops, savePath, 0);
+        this(accessKey, secretKey, configuration, bucket, pipeline, pfopJsonPath, pfopConfigs, fopsIndex, savePath, 0);
     }
 
-    private void set(String pipeline, String jsonPath, List<JsonObject> pfopConfigs, String fopsIndex, String fops)
-            throws IOException {
+    private void set(String pipeline, String jsonPath, List<JsonObject> pfopConfigs, String fopsIndex) throws IOException {
         this.pfopParams = new StringMap().putNotEmpty("pipeline", pipeline);
         if (jsonPath != null && !"".equals(jsonPath)) {
             this.pfopConfigs = new ArrayList<>();
@@ -56,17 +54,15 @@ public class QiniuPfop extends Base<Map<String, String>> {
             this.pfopConfigs = pfopConfigs;
         } else if (fopsIndex != null && !"".equals(fopsIndex)) {
             this.fopsIndex = fopsIndex;
-        } else if (fops != null && !"".equals(fops)) {
-            this.fops = fops;
         } else {
             throw new IOException("please set the pfop-config or fopsIndex or fops.");
         }
     }
 
-    public void updateFop(String bucket, String pipeline, String jsonPath, List<JsonObject> pfopConfigs, String fopsIndex,
-                          String fops) throws IOException {
+    public void updateFop(String bucket, String pipeline, String jsonPath, List<JsonObject> pfopConfigs, String fopsIndex)
+            throws IOException {
         this.bucket = bucket;
-        set(pipeline, jsonPath, pfopConfigs, fopsIndex, fops);
+        set(pipeline, jsonPath, pfopConfigs, fopsIndex);
     }
 
     public QiniuPfop clone() throws CloneNotSupportedException {
@@ -91,18 +87,15 @@ public class QiniuPfop extends Base<Map<String, String>> {
 
     @Override
     public String singleResult(Map<String, String> line) throws IOException {
-        if (pfopConfigs != null) {
+        if (pfopConfigs != null && pfopConfigs.size() > 0) {
             for (JsonObject pfopConfig : pfopConfigs) {
                 String cmd = PfopUtils.generateFopCmd(line.get("key"), pfopConfig);
                 fileSaveMapper.writeKeyFile(pfopConfig.get("name").getAsString(), line.get("key") + "\t" + cmd + "\t" +
                             operationManager.pfop(bucket, line.get("key"), cmd, pfopParams), false);
             }
-        } else if (fopsIndex != null && !"".equals(fopsIndex)) {
+        } else {
             fileSaveMapper.writeSuccess(line.get("key") + "\t" + line.get(fopsIndex) + "\t" +
                         operationManager.pfop(bucket, line.get("key"), line.get(fopsIndex), pfopParams), false);
-        } else {
-            fileSaveMapper.writeSuccess(line.get("key") + "\t" + fops + "\t" +
-                    operationManager.pfop(bucket, line.get("key"), fops, pfopParams), false);
         }
         return null;
     }
