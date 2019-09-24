8595b7528b964293ce69205d42572244520bf589
wubingheng <wubinghengajw@outlook.com>
2019-09-24 19:29:08 +0800

    optimize process Base and remove LogUtils.

--- src/main/java/com/qiniu/util/HttpRespUtils.java
+++ src/main/java/com/qiniu/util/HttpRespUtils.java
@@ -83,6 +83,29 @@ public final class HttpRespUtils {
     }
 
     /**
+     * 取出异常中的信息
+     * @param e 异常对象，不能为空
+     * @return 信息字符串
+     */
+    public static String getMessage(QiniuException e) {
+        // 取 error 信息优先从 exception 的 message 中取，避免直接 e.error() 抛出非预期异常，因为 error() 方法底层可能会调用
+        // response.isJson()，该方法底层会抛出空指针异常（位置 com.qiniu.http.Response.ctype(Response.java:137)），同时
+        // getMessage 会包含 reqid 等信息
+        String message = e.getMessage();
+        if (message == null || "".equals(message)) {
+            message = (e.response != null ? "reqid: " + e.response.reqId + ", ": "") + "code: " + e.code();
+            // 避免抛出空指针异常
+            try {
+                message += "， error: " + e.error();
+            } catch (Exception ex) {
+                message += ", failed: " + ex.getMessage();
+                ex.printStackTrace();
+            }
+        }
+        return message.replace("\n", "  ");
+    }
+
+    /**
      * 将 Response 对象转换成为结果字符串
      * @param response 得到的 Response 对象
      * @return Response body 转换的 String 对象
