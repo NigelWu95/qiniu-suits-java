85c808d48b43ebead6e249170305feac76dd3928
wubingheng <wubinghengajw@outlook.com>
2019-11-07 22:14:00 +0800

    optimize SuitsException stack and remove M3U8Utils.

--- src/main/java/com/qiniu/common/SuitsException.java
+++ src/main/java/com/qiniu/common/SuitsException.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class SuitsException extends IOException {
 
     private int statusCode;
+    private Exception exception;
 
     public SuitsException(int statusCode, String error) {
         super(String.join("", "code: ", String.valueOf(statusCode), ", error: ", error));
@@ -12,21 +13,28 @@ public class SuitsException extends IOException {
     }
 
     public SuitsException(Exception e, int statusCode) {
-        super(String.join(", ", String.valueOf(statusCode), e.getMessage()), e);
+        super(String.join(", ", String.valueOf(statusCode), e.getMessage()));
+        this.exception = e;
         this.statusCode = statusCode;
     }
 
     public SuitsException(Exception e, int statusCode, String error) {
-        super(String.join(", ", String.valueOf(statusCode), error, e.getMessage()), e);
+        super(String.join(", ", String.valueOf(statusCode), error, e.getMessage()));
+        this.exception = e;
         this.statusCode = statusCode;
     }
 
     public SuitsException(SuitsException e, String message) {
-        super(String.join(", ", e.getMessage(), message), e);
+        super(String.join(", ", e.getMessage(), message));
+        this.exception = e;
         this.statusCode = e.getStatusCode();
     }
 
     public int getStatusCode() {
         return statusCode;
+    }
+
+    public Exception getSuperException() {
+        return exception;
     }
 }
